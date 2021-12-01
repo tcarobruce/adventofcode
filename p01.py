881a20639c55dec220e07fc354ab923d90701a5a
Timothy Caro-Bruce <tcarobruce@gmail.com>
2021-11-30 22:04:22 -0800

    cuter

--- p01.py
+++ p01.py
@@ -5,5 +5,4 @@ nums = [int(ln.strip()) for ln in open(sys.argv[1])]
 print(sum([b > a for a, b in zip(nums[:-1], nums[1:])]))
 
 # part 2
-windows = [sum(t) for t in zip(nums[:-2], nums[1:-1], nums[2:])]
-print(sum([b > a for a, b in zip(windows[:-1], windows[1:])]))
+print(sum([b > a for a, b in zip(nums[:-3], nums[3:])]))
