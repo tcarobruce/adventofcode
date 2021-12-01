9aecf128bb46d92ba66bf490c48d9e7f33335927
Timothy Caro-Bruce <tcarobruce@gmail.com>
2021-11-30 22:01:29 -0800

    p1 part 2

--- p01.py
+++ p01.py
@@ -3,3 +3,7 @@ nums = [int(ln.strip()) for ln in open(sys.argv[1])]
 
 # part 1
 print(sum([b > a for a, b in zip(nums[:-1], nums[1:])]))
+
+# part 2
+windows = [sum(t) for t in zip(nums[:-2], nums[1:-1], nums[2:])]
+print(sum([b > a for a, b in zip(windows[:-1], windows[1:])]))
