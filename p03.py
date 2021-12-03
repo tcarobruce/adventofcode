f32a335daeea80c591f1bbe93457437676fc1f53
Timothy Caro-Bruce <tcarobruce@gmail.com>
2021-12-02 21:49:10 -0800

    p03 part 2 first pass

--- p03.py
+++ p03.py
@@ -1,13 +1,14 @@
 import sys
 
 lines = [
-    [int(i) for i in ln.strip()] for ln in open(sys.argv[1])
+    tuple([int(i) for i in ln.strip()]) for ln in open(sys.argv[1])
 ]
 
 # part 1
-totals = lines[0]
+width = len(lines[0])
+totals = [0] * width
 
-for i, ln in enumerate(lines, 1):
+for i, ln in enumerate(lines):
     for j, x in enumerate(ln):
         totals[j] += x
 
@@ -18,6 +19,7 @@ for bit in totals:
     gamma.append(int(bit > half))
     epsilon.append(int(bit < half))
 
+print(half, totals)
 print(gamma, epsilon)
 
 def to_int(ar):
@@ -25,4 +27,32 @@ def to_int(ar):
 
 print(to_int(gamma) * to_int(epsilon))
 
+# 841526
 
+# part 2
+
+oxygen = set(lines)
+co2 = set(lines)
+
+def most_common(nums, bit):
+    return int(sum([num[bit] for num in nums]) * 2 >= len(nums))
+
+bit = 0
+while len(oxygen) > 1:
+    mc = most_common(oxygen, bit)
+    oxygen = {num for num in oxygen if num[bit] == mc}
+    bit += 1
+
+oxygen = to_int(oxygen.pop())
+
+bit = 0
+while len(co2) > 1:
+    mc = 1 - most_common(co2, bit)
+    co2 = {num for num in co2 if num[bit] == mc}
+    bit += 1
+
+co2 = to_int(co2.pop())
+
+print(co2 * oxygen)
+
+# 4790390
