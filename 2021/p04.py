adb4c0e63ba84e90b457802e7734f2f6154a41a2
Timothy Caro-Bruce <tcarobruce@gmail.com>
2021-12-03 22:41:33 -0800

    refactor to a single function

--- 2021/p04.py
+++ 2021/p04.py
@@ -1,8 +1,15 @@
 import sys
 
-lines = [ln.strip() for ln in open(sys.argv[1])]
+lines = (ln.strip() for ln in open(sys.argv[1]))
 
-calls = [int(n) for n in lines.pop(0).split(",")]
+calls = [int(n) for n in next(lines).split(",")]
+row_groups = []
+for _ in lines:  # skips blank separator
+    rows = []
+    for _ in range(5):
+        rows.append([int(n) for n in next(lines).split()])
+    row_groups.append(rows)
+
 
 class Board:
     def __init__(self, rows):
@@ -17,25 +24,6 @@ class Board:
                 self.index[val] = (i, j)
                 self.total_unmarked += val
 
-    @classmethod
-    def read_boards(cls, lines):
-        boards = []
-        lines = iter(lines)
-        while True:
-            try:
-                boards.append(cls.read_board(lines))
-            except StopIteration:
-                break
-        return boards
-
-    @classmethod
-    def read_board(cls, lines):
-        next(lines)  # blank line
-        rows = []
-        for _ in range(5):
-            rows.append([int(n) for n in next(lines).split()])
-        return cls(rows)
-
     def handle_call(self, call):
         if call not in self.index:
             return
@@ -45,26 +33,25 @@ class Board:
         return all(self.marked[i]) or all(self.marked[y][j] for y in range(5))
 
 
-def find_first_winning_score(boards, calls):
-    for call in calls:
-        for board in boards:
-            result = board.handle_call(call)
-            if result:
-                return board.total_unmarked * call
-
-# part 1: 38594
-boards = Board.read_boards(lines)
-print(find_first_winning_score(boards, calls))
-
-# part 2: 21184
-def find_last_winning_score(boards, calls):
+def find_wins(row_groups, calls):
+    boards = [Board(rows) for rows in row_groups]
     calls = iter(calls)
     while boards:
         call = next(calls)
-        board = boards[0]  # save in case it's the last one
-        boards = [b for b in boards if not b.handle_call(call)]
-    return board.total_unmarked * call
+        next_boards = []
+        for board in boards:
+            win = board.handle_call(call)
+            if win:
+                yield board.total_unmarked * call
+            else:
+                next_boards.append(board)
+        boards = next_boards
 
 
-boards = Board.read_boards(lines)
-print(find_last_winning_score(boards, calls))
+# part 1: 38594
+print(next(find_wins(row_groups, calls)))
+
+# part 2: 21184
+for win in find_wins(row_groups, calls):
+    pass
+print(win)
