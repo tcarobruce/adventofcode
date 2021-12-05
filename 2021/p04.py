4ae2f87eae7e8d5197b480ce9814074fa09084ec
Timothy Caro-Bruce <tcarobruce@gmail.com>
2021-12-05 15:35:09 -0800

    just use index

--- 2021/p04.py
+++ 2021/p04.py
@@ -15,7 +15,6 @@ class Board:
     def __init__(self, rows):
         self.rows = rows
         self.index = {}
-        self.total_unmarked = 0
         self.marked_rows = [0] * 5
         self.marked_cols = [0] * 5
 
@@ -23,15 +22,13 @@ class Board:
             for j, val in enumerate(row):
                 assert val not in self.index, "Dupes not allowed!"
                 self.index[val] = (i, j)
-                self.total_unmarked += val
 
     def handle_call(self, call):
         if call not in self.index:
             return
-        i, j = self.index[call]
+        i, j = self.index.pop(call)
         self.marked_rows[i] += 1
         self.marked_cols[j] += 1
-        self.total_unmarked -= call
         return self.marked_rows[i] == 5 or self.marked_cols[j] == 5
 
 
@@ -44,7 +41,7 @@ def find_wins(row_groups, calls):
         for board in boards:
             win = board.handle_call(call)
             if win:
-                yield board.total_unmarked * call
+                yield sum(board.index.keys()) * call
             else:
                 next_boards.append(board)
         boards = next_boards
