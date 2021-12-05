05694c15c32d1e7c77a99706606c5a7cf81b37c7
Timothy Caro-Bruce <tcarobruce@gmail.com>
2021-12-05 15:43:21 -0800

    factor down line_points

--- 2021/p05.py
+++ 2021/p05.py
@@ -1,6 +1,6 @@
 import sys
 from collections import Counter
-from itertools import zip_longest
+from itertools import cycle
 
 lines = []
 for ln in open(sys.argv[1]):
@@ -12,37 +12,27 @@ for ln in open(sys.argv[1]):
         )
     )
 
-def ranger(a, b):
-    r = range(min(a, b), max(a, b) + 1)
-    if a > b:
-        r = reversed(r)
-    return r
 
-def line_points(start, finish):
-    fillvalue = None
-    xs, ys = (), ()
-    if start[0] == finish[0]:
-        fillvalue = start[0]
-    else:
-        xs = ranger(start[0], finish[0])
-    if start[1] == finish[1]:
-        fillvalue = start[1]
-    else:
-        ys = ranger(start[1], finish[1])
-    return zip_longest(xs, ys, fillvalue=fillvalue)
+def ranger(a, b):
+    if a == b:
+        return cycle([a])
+    direction = 1 if a < b else -1
+    return range(a, b + direction, direction)
 
 
 straight_counts = Counter()
 all_counts = Counter()
 for start, finish in lines:
     straight = (start[0] == finish[0]) or (start[1] == finish[1])
-    for pt in line_points(start, finish):
+    line_points = zip(ranger(start[0], finish[0]), ranger(start[1], finish[1]))
+    for pt in line_points:
         all_counts[pt] += 1
         if straight:
             straight_counts[pt] += 1
 
+
 # part 1: 7414
 print(len([v for v in straight_counts.values() if v > 1]))
 
-# part 2
+# part 2: 19676
 print(len([v for v in all_counts.values() if v > 1]))
