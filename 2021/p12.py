40215509e68a365c07f6129504ea40c2ec4e0185
Timothy Caro-Bruce <tcarobruce@gmail.com>
2021-12-11 22:01:30 -0800

    refactor down

--- 2021/p12.py
+++ 2021/p12.py
@@ -4,10 +4,8 @@ from string import ascii_uppercase
 
 upcase = set(ascii_uppercase)
 
-
 def is_big(s):
     return set(s) < upcase
-
 
 edges = defaultdict(list)
 
@@ -17,40 +15,22 @@ for ln in open(sys.argv[1]):
     edges[b].append(a)
 
 
-def paths_to_end(edges, node="start", seen=set()):
-    seen = seen.copy()
-    if node in seen:
-        return 0
-    elif node == "end":
+def paths_to_end(edges, node="start", seen=set(), allow_repeat=False):
+    if node == "end":
         return 1
+    if node in seen and (not allow_repeat or node == "start"):
+        return 0
+    elif node in seen:
+        allow_repeat = False
     if not is_big(node):
-        seen.add(node)
+        seen = seen | {node}
     return sum([
-        paths_to_end(edges, n, seen)
+        paths_to_end(edges, n, seen, allow_repeat=allow_repeat)
         for n in edges[node]
     ])
 
 
 # part 1: 3576
 print(paths_to_end(edges))
-
-
-def paths_to_end_twosmalls(edges, node="start", seen=set(), seen_once=None):
-    seen = seen.copy()
-    if node == "end":
-        return 1
-    if node in seen and (seen_once or node in ("start", "end")):
-        return 0
-    elif node in seen:
-        return sum([
-            paths_to_end_twosmalls(edges, n, seen, seen_once=node)
-            for n in edges[node]
-        ])
-    if not is_big(node):
-        seen.add(node)
-    return sum([
-        paths_to_end_twosmalls(edges, n, seen, seen_once=seen_once)
-        for n in edges[node]
-    ])
-
-print(paths_to_end_twosmalls(edges))
+# part 2: 84271
+print(paths_to_end(edges, allow_repeat=True))
