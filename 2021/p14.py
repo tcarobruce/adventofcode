8e5386d0da54e3491a6120b8c6b35bff3597b4b3
Timothy Caro-Bruce <tcarobruce@gmail.com>
2021-12-13 22:11:44 -0800

    duh, maxsize

--- 2021/p14.py
+++ 2021/p14.py
@@ -9,10 +9,10 @@ polymer = list(lines[0])
 rules = dict(ln.split(' -> ') for ln in lines[2:])
 
 
-@lru_cache
+@lru_cache(maxsize=None)
 def counts(a, c, its):
     if its == 0:
-        return Counter([a])
+        return Counter([a])  # don't double-count c
     b = rules[a + c]
     return counts(a, b, its-1) + counts(b, c, its-1)
 
@@ -20,11 +20,12 @@ def counts(a, c, its):
 def most_least_diff(polymer, iterations):
     mc = sum(
         [counts(a, b, iterations) for a, b in zip(polymer[:-1], polymer[1:])],
-        Counter(polymer[-1]),
+        Counter(polymer[-1]),  # count last letter
     ).most_common()
-    print(mc)
     return mc[0][1] - mc[-1][1]
 
 
 # part 1: 2988
 print(most_least_diff(polymer, 10))
+# part 2: 3572761917024
+print(most_least_diff(polymer, 40))
