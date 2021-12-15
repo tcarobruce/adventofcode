97edb6f0ad138bf84dabdb9b5966668ad3a7a493
Timothy Caro-Bruce <tcarobruce@gmail.com>
2021-12-14 21:55:12 -0800

    day 15 part 2

--- 2021/p15.py
+++ 2021/p15.py
@@ -6,7 +6,6 @@ for y, ln in enumerate(open(sys.argv[1])):
     for x, cost in enumerate(ln.strip()):
         grid[(x, y)] = int(cost)
 
-dest = x, y
 
 def neighbors(x, y):
     yield x - 1, y
@@ -15,17 +14,38 @@ def neighbors(x, y):
     yield x, y + 1
 
 
-def least_risk_path(start, grid, dest):
+def least_risk_path(grid):
+    grid = dict(grid)
+    start = (0, 0)
+    dest = max([x for x, _ in grid]), max([y for _, y in grid])
     q = [(0, start, [start])]
     grid.pop(start)
+
     while True:
         cost, pos, chain = heappop(q)
         if pos == dest:
-            print(chain)
+            #print(chain)
             return cost
         for n in neighbors(*pos):
             if n not in grid:
                 continue
             heappush(q, (cost + grid.pop(n), n, chain + [n]))
 
-print(least_risk_path((0, 0), grid, dest))
+
+# part 1: 447
+print(least_risk_path(grid))
+
+def expand_grid(grid, n=5):
+    expanded = {}
+    width = max([x for x, _ in grid]) + 1
+    height = max([y for _, y in grid]) + 1
+    for x, y in grid:
+        for xoff in range(n):
+            for yoff in range(n):
+                nx = x + (xoff * width)
+                ny = y + (yoff * height)
+                expanded[(nx, ny)] = (grid[(x, y)] - 1 + xoff + yoff) % 9 + 1
+    return expanded
+
+# part 2: 2825
+print(least_risk_path(expand_grid(grid)))
