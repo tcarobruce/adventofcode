5392d4f7505af540af40265a641308d60c3008d6
Timothy Caro-Bruce <tcarobruce@gmail.com>
2022-12-03 09:17:27 -0800

    part 1 cleanup

--- 2022/p02.py
+++ 2022/p02.py
@@ -2,24 +2,13 @@ import sys
 
 plays = [ln.strip().split(" ") for ln in open(sys.argv[1])]
 
-abc = "ABC"
-xyz = "XYZ"
-
-scores = {"X": 1, "Y": 2, "Z": 3}
 results = [3, 6, 0]
 
 def score_row(row):
     theirs, mine = row
-    result = xyz.index(mine) - abc.index(theirs)
-    return scores[mine] + (results[result % 3])
+    my_index = "XYZ".index(mine)
+    result = my_index - "ABC".index(theirs)
+    return my_index + 1 + (results[result % 3])
 
 
-
-tot = 0
-for p in plays:
-    s = score_row(p)
-    print(p, s)
-    tot += s
-
-print(tot)
-
+print(sum([score_row(p) for p in plays]))  # 13221
