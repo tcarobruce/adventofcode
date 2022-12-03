4252ff7c15717db9794a0d2e00367dc1d5b1f5b7
Timothy Caro-Bruce <tcarobruce@gmail.com>
2022-12-03 09:17:20 -0800

    cleanup

--- 2022/p01.py
+++ 2022/p01.py
@@ -2,15 +2,13 @@ import sys
 
 groups = open(sys.argv[1]).read().split("\n\n")
 
-calories = []
-for group in groups:
-    calories.append(
-        sum(
-            [
-                int(ln.strip()) for ln in group.split("\n") if ln.strip()
-            ]
-        )
-    )
+calories = [0]
+for ln in open(sys.argv[1]):
+    ln = ln.strip()
+    if not ln:
+        calories.append(0)
+        continue
+    calories[-1] += int(ln)
 
 calories.sort()
 print(calories[-1])
