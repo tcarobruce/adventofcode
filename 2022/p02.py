ca95a612b542af50d534fc0e80c3cbc3aa901880
Timothy Caro-Bruce <tcarobruce@gmail.com>
2022-12-03 09:17:34 -0800

    use a single result array, clean up

--- 2022/p02.py
+++ 2022/p02.py
@@ -2,26 +2,21 @@ import sys
 
 plays = [ln.strip().split(" ") for ln in open(sys.argv[1])]
 
-results = [3, 6, 0]
+results = [0, 3, 6]
 
-def score_row(row):
-    theirs, mine = row
+def score_row(theirs, mine):
     my_index = "XYZ".index(mine)
-    result = my_index - "ABC".index(theirs)
-    return my_index + 1 + (results[result % 3])
+    result_index = my_index - "ABC".index(theirs) + 1
+    return my_index + 1 + (results[result_index % 3])
 
 
-print(sum([score_row(p) for p in plays]))  # 13221
+print(sum([score_row(*p) for p in plays]))  # 13221
 
 
-offsets = [-1, 0, 1]
-results2 = [0, 3, 6]
-
-def score_row2(row):
-    theirs, result = row
+def score_row2(theirs, result):
     result_index = "XYZ".index(result)
-    mine = ("ABC".index(theirs) + offsets[result_index]) % 3
-    return mine + 1 + results2[result_index]
+    my_index = ("ABC".index(theirs) + result_index - 1) % 3
+    return my_index + 1 + results[result_index]
 
 
-print(sum([score_row2(p) for p in plays]))  # 13131
+print(sum([score_row2(*p) for p in plays]))  # 13131
