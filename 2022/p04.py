1e60b5b592125fb0746f56417483f35122ee78b9
Timothy Caro-Bruce <tcarobruce@gmail.com>
2022-12-03 21:12:38 -0800

    p4-2

--- 2022/p04.py
+++ 2022/p04.py
@@ -1,14 +1,15 @@
 import sys
 
 lines = [ln.strip().split(",") for ln in open(sys.argv[1])]
-sections = []
 tot = 0
+p2 = 0
 for a, b in lines:
     al, ah = [int(c) for c in a.split("-")]
     bl, bh = [int(c) for c in b.split("-")]
     if (al <= bl and ah >= bh) or (al >= bl and ah <= bh):
         tot += 1
-    sections.append(((al, ah), (bl, bh)))
+    if (ah >= bl and al <= bh) or (al <= bh and ah >= bl):
+        p2 += 1
 
-print(sections)
 print(tot)
+print(p2)
