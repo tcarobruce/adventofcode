9b6586364a570a407d3b2bcb3b46c1281d7c3ec4
Timothy Caro-Bruce <tcarobruce@gmail.com>
2022-12-04 21:39:38 -0800

    some cleanup

--- 2022/p05.py
+++ 2022/p05.py
@@ -1,36 +1,45 @@
 import sys
 import re
+from copy import deepcopy
 
 f = iter(open(sys.argv[1]))
-stack_count = int(sys.argv[2])
 
-stacks = [[] for _ in range(stack_count)]
-
-
+stacks = None
+stack_count = None
 
 for line in f:
-    if line[1] == "1":
-        next(f)
+    if stacks is None:
+        stack_count = len(line) // 4
+        stacks = [[] for _ in range(stack_count)]
+    if "[" not in line:
+        next(f)  # skip blank line
         break
-    for s in range(stack_count):
-        c = line[s * 4 + 1]
-        if c != ' ':
-            stacks[s].insert(0, c)
+    stack = 0
+    for stack in range(stack_count):
+        c = line[stack * 4 + 1]
+        if c != " ":
+            stacks[stack].insert(0, c)
 
 patt = re.compile(r'move (\d+) from (\d+) to (\d+)')
+moves = []
 
-part1 = False
 for line in f:
     m = patt.match(line)
     count, src, dest = [int(x) for x in m.groups()]
-    src -= 1
-    dest -= 1
-    if part1:
-        for _ in range(count):
-            stacks[dest].append(stacks[src].pop())
-    else:
-        stacks[dest] += stacks[src][-count:]
-        stacks[src] = stacks[src][:-count]
+    moves.append((src - 1, dest - 1, count))  # zero-index
 
-print(stacks)
+orig = stacks
+
+# part 1
+stacks = deepcopy(orig)
+for src, dest, count in moves:
+    for _ in range(count):
+        stacks[dest].append(stacks[src].pop())
+print("".join([s[-1] for s in stacks]))
+
+# part 2
+stacks = deepcopy(orig)
+for src, dest, count in moves:
+    stacks[dest] += stacks[src][-count:]
+    stacks[src] = stacks[src][:-count]
 print("".join([s[-1] for s in stacks]))
