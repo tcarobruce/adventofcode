bda592e69341568396ed1a5b1e326c5909cb0821
Timothy Caro-Bruce <tcarobruce@gmail.com>
2022-12-04 21:55:19 -0800

    shorten up

--- 2022/p05.py
+++ 2022/p05.py
@@ -2,32 +2,22 @@ import sys
 import re
 from copy import deepcopy
 
-f = open(sys.argv[1])
-
-stack_input, move_input = f.read().split("\n\n")
+stack_input, move_input = open(sys.argv[1]).read().split("\n\n")
 crates = [ln[1::4] for ln in stack_input.splitlines()[:-1]]
 stacks = [[c for c in t[::-1] if c != " "] for t in zip(*crates)]
+stacks2 = deepcopy(stacks)
 
 patt = re.compile(r'move (\d+) from (\d+) to (\d+)')
-moves = []
 
 for line in move_input.splitlines():
     m = patt.match(line)
     count, src, dest = [int(x) for x in m.groups()]
-    moves.append((src - 1, dest - 1, count))  # zero-index
-
-orig = stacks
-
-# part 1
-stacks = deepcopy(orig)
-for src, dest, count in moves:
+    src -= 1
+    dest -= 1
     for _ in range(count):
         stacks[dest].append(stacks[src].pop())
-print("".join([s[-1] for s in stacks]))
+    stacks2[dest] += stacks2[src][-count:]
+    stacks2[src] = stacks2[src][:-count]
 
-# part 2
-stacks = deepcopy(orig)
-for src, dest, count in moves:
-    stacks[dest] += stacks[src][-count:]
-    stacks[src] = stacks[src][:-count]
-print("".join([s[-1] for s in stacks]))
+for ss in [stacks, stacks2]:
+    print("".join([s[-1] for s in ss]))
