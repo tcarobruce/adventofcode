a7f6602721699b4de140e39a372531d44c8784af
Timothy Caro-Bruce <tcarobruce@gmail.com>
2022-12-07 14:02:31 -0800

    keep just dir sizes by path and strip out extraneous stuff

--- 2022/p07.py
+++ 2022/p07.py
@@ -1,52 +1,35 @@
 import sys
+from collections import defaultdict
 
 lines = open(sys.argv[1]).read().splitlines()
 
-root = {}
-dirstack = [root]
-current_dir = root
+dirstack = ["/"]
+dirs = defaultdict(int)
 
 for line in lines:
     parts = line.split()
     if parts[:2] == ["$", "cd"]:
         d = parts[2]
         if d == "/":
-            dirstack = [root]
-            current_dir = root
+            dirstack = ["/"]
         elif d == "..":
             dirstack.pop()
-            current_dir = dirstack[-1]
         else:
-            current_dir = current_dir[d]
-            dirstack.append(current_dir)
-    elif parts[:2] == ["$", "ls"]:
-        continue
-    elif parts[0] == "dir":
-        current_dir.setdefault(parts[1], {})
+            dirstack.append(d + "/")
+
     elif parts[0].isdigit():
         size = int(parts[0])
+        dd = ""
         for d in dirstack:
-            d.setdefault("__TOTAL__", 0)
-            d["__TOTAL__"] += size
-    else:
-        assert False, "unexpected ls: " + line
-
-
-def visit(d):
-    for k, v in d.items():
-        if k == "__FILES__":
-            continue
-        elif k == "__TOTAL__":
-            yield v
-        else:
-            yield from visit(v)
+            dd += d
+            dirs[dd] += size
 
 
 smalldir_limit = 100000
 disk = 70000000
 needed = 30000000
 
-sizes = list(visit(root))
+sizes = list(dirs.values())
 available = disk - max(sizes)
 
 print(sum([s for s in sizes if s < smalldir_limit]))
