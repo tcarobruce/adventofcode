f660b7f2f8b0df0d4f88c97931475cc62d24c64f
Timothy Caro-Bruce <tcarobruce@gmail.com>
2022-12-09 21:51:09 -0800

    cleanup

--- 2022/p10.py
+++ 2022/p10.py
@@ -1,38 +1,34 @@
 import sys
-from collections import defaultdict
 
 lines = open(sys.argv[1]).read().splitlines()
-
-moments = set(range(20, 221, 40))
 
 
 def process(lines):
     X = 1
-    cycle = 1
     for line in lines:
         parts = line.split()
         if parts[0] == "noop":
-            cycle += 1
-            yield cycle, X
+            yield X
         elif parts[0] == "addx":
-            cycle += 1
-            yield cycle, X
-            cycle += 1
+            yield X
+            yield X
             X += int(parts[1])
-            yield cycle, X
-
 
 tot = 0
-print("#", end="")
-for cycle, X in process(lines):
-    if cycle in moments:
+moments = set(range(20, 221, 40))
+
+for cycle, X in enumerate(process(lines), 1):
+    # part 1
+    if (cycle + 20) % 40 == 0:
         tot += X * cycle
-    if (cycle - 1) % 40 == 0:
-        print()
-    if abs(X - ((cycle - 1)% 40)) <= 1:
-        print("#", end="")
-    else:
-        print(".", end="")
+
+    # part 2
+    c, end = ".", ""
+    if abs(X - ((cycle - 1) % 40)) <= 1:
+        c = "#"
+    if (cycle) % 40 == 0:
+        end = "\n"
+    print(c, end=end)
 
 print()
 print(tot)
