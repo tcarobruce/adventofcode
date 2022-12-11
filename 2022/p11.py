380608542fbd0dff0ee5b6f1b9fb379f942bd890
Timothy Caro-Bruce <tcarobruce@gmail.com>
2022-12-10 21:46:14 -0800

    11-2

--- 2022/p11.py
+++ 2022/p11.py
@@ -1,16 +1,22 @@
 import sys
 from operator import *
+from functools import partial
+from math import prod
 
 OPS = {
     "*": mul,
     "+": add,
 }
 
+def square(n):
+    return n * n
+
 
 class Monkey:
-    def __init__(self, items, op, throw):
+    def __init__(self, items, op, divis, throw):
         self.items = items
         self.op = op
+        self.divis = divis
         self.throw = throw
         self.handled = 0
 
@@ -19,9 +25,11 @@ class Monkey:
         parts = next(lines).split(":")
         items = [int(d) for d in parts[1].strip().split(",")]
         op = next(lines).split("new = old ")[1].split(" ")
-        op[0] = OPS[op[0]]
+        fn = OPS[op[0]]
         if op[1].isdigit():
-            op[1] = int(op[1])
+            op = partial(fn, int(op[1]))
+        else:
+            op = square
         divis = int(next(lines).split()[-1])
         iftrue = int(next(lines).split()[-1])
         iffalse = int(next(lines).split()[-1])
@@ -29,21 +37,17 @@ class Monkey:
         def throw(n):
             return iffalse if n % divis else iftrue
 
-        return cls(items, op, throw)
+        return cls(items, op, divis, throw)
 
     def inspect(self, item):
-        fn = self.op[0]
-        operand = self.op[1]
-        if operand == "old":
-            operand = item
-        return fn(item, operand)
+        return self.op(item) % cap
 
     def receive(self, item):
         self.items.append(item)
 
     def handle_one(self, item, monkeys):
         item = self.inspect(item)
-        item //= 3
+        #item //= 3
         dest = self.throw(item)
         monkeys[dest].receive(item)
         self.handled += 1
@@ -55,14 +59,6 @@ class Monkey:
 
 
 
-
-
-
-
-
-
-
-
 lines = open(sys.argv[1]).read().splitlines()
 lines = (ln for ln in lines if ln)
 monkeys = []
@@ -71,12 +67,19 @@ for line in lines:
     monkeys.append(Monkey.from_lines(lines))
 
 
-for _ in range(20):
+cap = prod([m.divis for m in monkeys])
+
+
+for r in range(10000):
+    print(r)
     for m in monkeys:
         m.handle_turn(monkeys)
 
-for i, m in enumerate(monkeys):
-    print(f"Monkey {i} inspected items {m.handled} times.")
+    if ((r + 1) % 1000 == 0):
+        for i, m in enumerate(monkeys):
+            print(f"Monkey {i} inspected items {m.handled} times. {len(m.items)}")
+        print()
+        input()
 
 monkeys.sort(key=lambda m: -m.handled)
 print(monkeys[0].handled * monkeys[1].handled)
