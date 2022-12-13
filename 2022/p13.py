6347735bdee131244c10ce762e3a07592813a2b9
Timothy Caro-Bruce <tcarobruce@gmail.com>
2022-12-12 21:37:55 -0800

    cleanup

--- 2022/p13.py
+++ 2022/p13.py
@@ -1,11 +1,15 @@
 import sys
 from itertools import zip_longest
 from functools import cmp_to_key
+from math import prod
 
 FILL = object()
 
 doc = open(sys.argv[1]).read()
-pairs = [[eval(ln) for ln in p.strip().split("\n")] for p in doc.split("\n\n")]
+pairs = [
+    [eval(ln) for ln in p.strip().split("\n")]
+    for p in doc.split("\n\n")
+]
 
 def cmp(a, b):
     return (a > b) - (a < b)
@@ -13,35 +17,25 @@ def cmp(a, b):
 def compare(a, b):
     if isinstance(a, int) and isinstance(b, int):
         return cmp(a, b)
-    if isinstance(a, list) and isinstance(b, list):
+    elif isinstance(a, list) and isinstance(b, list):
         for ai, bi in zip_longest(a, b, fillvalue=FILL):
             if ai == FILL:
                 return -1
             elif bi == FILL:
                 return 1
-            r = compare(ai, bi)
-            if r != 0:
+            elif r := compare(ai, bi):
                 return r
         return 0
-    if isinstance(a, int) and isinstance(b, list):
+    elif isinstance(a, int) and isinstance(b, list):
         return compare([a], b)
-    if isinstance(a, list) and isinstance(b, int):
+    elif isinstance(a, list) and isinstance(b, int):
         return compare(a, [b])
     assert False, "bad juju %r %r" % (a, b)
 
 
-total = 0
-for i, (a, b) in enumerate(pairs, 1):
-    if compare(a, b) < 0:
-        total += i
-print(total)
+print(sum([i for i, (a, b) in enumerate(pairs, 1) if compare(a, b) < 0]))
 
 distress = [[[2]], [[6]]]
 all_lines = sum(pairs, distress)
 all_lines.sort(key=cmp_to_key(compare))
-
-result = 1
-for i, ln in enumerate(all_lines, 1):
-    if ln in distress:
-        result *= i
-print(result)
+print(prod([1 + all_lines.index(d) for d in distress]))
