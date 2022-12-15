a00b9e9ca091f3352308ff9aaa65a1270a5dab33
Timothy Caro-Bruce <tcarobruce@gmail.com>
2022-12-15 10:00:52 -0800

    some optimizations to get to 30s

--- 2022/p15.py
+++ 2022/p15.py
@@ -1,5 +1,6 @@
 import sys
 import re
+from operator import itemgetter
 
 lines = open(sys.argv[1]).read().splitlines()
 row = int(sys.argv[2])
@@ -10,31 +11,36 @@ p = re.compile(r"Sensor at x=([\d-]+), y=([\d-]+): closest beacon is at x=([\d-]
 
 for line in lines:
     m = p.match(line)
-    sensors.append([int(s) for s in m.groups()])
+    sx, sy, bx, by = [int(s) for s in m.groups()]
+    dist = abs(sx - bx) + abs(sy - by)
+    sensors.append((sx, sy, dist))
 
-def dist(sensor):
-    sx, sy, bx, by = sensor
-    return abs(sx - bx) + abs(sy - by)
+
+sensors.sort(key=itemgetter(1))  # help to sort by y?
+
 
 def range_for_row(sensor, row):
-    sx, sy, bx, by = sensor
-    d = dist(sensor)
+    sx, sy, dist = sensor
     ydist = abs(sy - row)
-    if ydist > d:
+    if ydist > dist:
         return None
-    xrad = d - ydist
-    return range(sx - xrad, sx + xrad + 1)
+    xrad = dist - ydist
+    return (sx - xrad, sx + xrad + 1)
 
 
 def overlap(r1, r2):
-    return max(r1[0], r2[0]) < min(r1.stop, r2.stop)
+    return max(r1[0], r2[0]) < min(r1[-1], r2[-1])
 
 def add_ranges(r1, r2):
     # assumed overlapping
-    return range(min(r1.start, r2.start), max(r1.stop, r2.stop))
+    return (min(r1[0], r2[0]), max(r1[-1], r2[-1]))
 
 
-def find_ranges(row):
+def contains(r1, r2):
+    return r1[0] <= r2[0] and r1[-1] >= r2[-1]
+
+
+def find_ranges(row, lim=None):
     ranges = []
     for sensor in sensors:
         next_ranges = []
@@ -46,19 +52,21 @@ def find_ranges(row):
                 rr = add_ranges(rr, ro)
             else:
                 next_ranges.append(ro)
+        if lim is not None and contains(rr, lim):
+            return [rr]
         next_ranges.append(rr)
         ranges = next_ranges
     return ranges
 
 
-print(sum([len(r) - 1 for r in find_ranges(row)]))
+print(sum([(r[-1] - r[0]) - 1 for r in find_ranges(row)]))
 
 for row in range(0, 4000001):
-    fr = find_ranges(row)
+    fr = find_ranges(row, lim=range(0, 4000001))
     if len(fr) > 1:
         print(row, fr)
         break
     if row % 100000 == 0:
         print(row)
 
-print(4000000 * fr[0].stop + row)
+print(4000000 * fr[0][-1] + row)
