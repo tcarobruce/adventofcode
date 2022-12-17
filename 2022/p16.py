0f3c86a1747b13df7a81a45c9c3647bf82bb22d7
Timothy Caro-Bruce <tcarobruce@gmail.com>
2022-12-16 20:52:40 -0800

    slow, but working part 2

--- 2022/p16.py
+++ 2022/p16.py
@@ -1,6 +1,7 @@
 import sys
 import re
 from functools import lru_cache
+from itertools import product
 from heapq import heappush, heappop
 
 valves = {}
@@ -16,15 +17,23 @@ for line in lines:
     valves[valve] = int(flow)
     connections[valve] = dests.split(", ")
 
+def combine(me, el, open_valves, flow_rate):
+    me, me_action = me
+    el, el_action = el
 
 def find_best_release(time_limit=30):
     # minutes, -released, flow_rate, valve, open_valves
-    q = [(0, 0, 0, "AA", set())]
+    q = [(0, 0, 0, "AA", "AA", set())]
 
     seen = set()
 
+    last = 0
+
     while True:
-        minutes, released, flow_rate, valve, open_valves = heappop(q)
+        minutes, released, flow_rate, me, el, open_valves = heappop(q)
+        if last < minutes:
+            last = minutes
+            print(last, len(q))
 
         if minutes == time_limit:
             return -released
@@ -32,18 +41,39 @@ def find_best_release(time_limit=30):
         minutes += 1
         released -= flow_rate
 
-        k = (valve, released)
+        k = (me, el, released)
         if k in seen:
             continue
 
-        if valve not in open_valves and valves[valve] > 0:
-            # open the valve
-            heappush(q, (minutes, released, flow_rate + valves[valve], valve, open_valves | {valve}))
+        me_actions = []
+        el_actions = []
+        if me not in open_valves and valves[me] > 0:
+            me_actions.append(("open", me, me))
 
-        for conn in connections[valve]:
-            heappush(q, (minutes, released, flow_rate, conn, open_valves))
+        if el not in open_valves and valves[el] > 0 and me != el:
+            # open the valve
+            el_actions.append(("open", el, el))
+
+        for conn in connections[me]:
+            me_actions.append(("move", me, conn))
+        for conn in connections[el]:
+            el_actions.append(("move", el, conn))
+
+        for (me_action, me_start, me_dest), (el_action, el_start, el_dest) in product(me_actions, el_actions):
+            fr = flow_rate
+            ov = open_valves
+            if me_action == "open":
+                fr += valves[me_start]
+                ov = ov | {me_start}
+
+            if el_action == "open":
+                fr += valves[el_start]
+                ov = ov | {el_start}
+
+            heappush(q, (minutes, released, fr, me_dest, el_dest, ov))
 
         seen.add(k)
 
 
-print(find_best_release())
+#print(find_best_release())
+print(find_best_release(26))
