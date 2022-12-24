3ff57de8dd54565527e70351164d7963ef80c392
Timothy Caro-Bruce <tcarobruce@gmail.com>
2022-12-24 14:36:07 -0800

    p20 part 2

--- 2022/p20.py
+++ 2022/p20.py
@@ -16,14 +16,18 @@ for num in nums:
         last.order = node
     last = node
 
-node = nodes[0]
-while node is not None:
-    pos = nodes.index(node)
-    v = node.value
-    nodes = nodes[:pos] + nodes[pos + 1:]
-    dest = ((pos + node.value) % len(nodes)) or len(nodes)
-    nodes = nodes[:dest] + [node] + nodes[dest:]
-    node = node.order
+def mix(nodes, first):
+    node = first
+    while node is not None:
+        pos = nodes.index(node)
+        v = node.value
+        nodes = nodes[:pos] + nodes[pos + 1:]
+        dest = ((pos + node.value) % len(nodes)) or len(nodes)
+        nodes = nodes[:dest] + [node] + nodes[dest:]
+        node = node.order
+    return nodes
+
+nodes = mix(nodes, nodes[0])
 
 tot = 0
 for zero_pos, node in enumerate(nodes):
@@ -33,4 +37,30 @@ for zero_pos, node in enumerate(nodes):
 for n in [1000, 2000, 3000]:
     tot += nodes[(n + zero_pos) % len(nodes)].value
 
+print(tot)
+
+last = None
+first = None
+nodes = []
+key = 811589153
+for num in nums:
+    node = Node(num * key)
+    if first is None:
+        first = node
+    nodes.append(node)
+    if last is not None:
+        last.order = node
+    last = node
+
+
+for _ in range(10):
+    nodes = mix(nodes, first)
+
+tot = 0
+for zero_pos, node in enumerate(nodes):
+    if node.value == 0:
+        break
+
+for n in [1000, 2000, 3000]:
+    tot += nodes[(n + zero_pos) % len(nodes)].value
 print(tot)
