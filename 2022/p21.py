bbf8f62018d9f724d203fc292b829efa67838534
Timothy Caro-Bruce <tcarobruce@gmail.com>
2022-12-25 13:44:52 -0800

    21-2

--- 2022/p21.py
+++ 2022/p21.py
@@ -1,5 +1,4 @@
 import sys
-from collections import deque
 from operator import add, sub, mul, truediv, floordiv
 
 OPS = {
@@ -22,14 +21,66 @@ for ln in lines:
         maths[monkey] = rest.split()
 
 
+orig_nums = dict(nums)
+
 def resolve(m):
     v = nums.get(m)
     if v is not None:
         return v
     left, op, right = maths[m]
-    v = OPS[op](resolve(left), resolve(right))
-    nums[m] = v
-    return v
+    return OPS[op](resolve(left), resolve(right))
 
 
 print(resolve("root"))
+
+nums.pop("humn")
+maths["root"][1] = "=="
+
+def reconcile(m, result):
+    if m == "humn":
+        print(result)
+        return None
+
+    left, op, right = maths[m]
+    try:
+        left_result = resolve(left)
+    except:
+        left_result = None
+
+    try:
+        right_result = resolve(right)
+    except:
+        right_result = None
+
+    if op == "==":
+        if left_result is None:
+            reconcile(left, right_result)
+        if right_result is None:
+            reconcile(right, left_result)
+
+    if op == "+":
+        if left_result is None:
+            reconcile(left, result - right_result)
+        elif right_result is None:
+            reconcile(right, result - left_result)
+
+    if op == "*":
+        if left_result is None:
+            reconcile(left, result // right_result)
+        elif right_result is None:
+            reconcile(right, result // left_result)
+
+    if op == "-":
+        if left_result is None:
+            reconcile(left, result + right_result)
+        elif right_result is None:
+            reconcile(right, left_result - result)
+
+    if op == "/":
+        if left_result is None:
+            reconcile(left, result * right_result)
+        elif right_result is None:
+            reconcile(right, left_result // result)
+
+
+reconcile("root", 0)
