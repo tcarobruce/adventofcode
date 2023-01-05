8aa19abd18184581ca011186ca1e6790faf11f82
Timothy Caro-Bruce <tcarobruce@gmail.com>
2023-01-04 20:25:17 -0800

    22-1

--- 2022/p22.py
+++ 2022/p22.py
@@ -3,84 +3,65 @@ import re
 
 lines = open(sys.argv[1]).read().splitlines()
 
-directions = re.findall("(\d+|[LR])", lines.pop())
-
+path = re.findall(r"(\d+|[LR])", lines.pop())
 lines.pop()
 
 grid = {}
-minx = {}
-maxx = {}
-miny = {}
-maxy = {}
+warp = {}
+ymins = {}
+ymaxs = {}
 
 pos = None
-facing = (1, 0)
+directions = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+facing = directions[0]
 
-dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
-
-
-def rotate(facing, d):
-    return dirs[(dirs.index(facing) + {"L": -1, "R": 1}[d]) % 4]
-
-
-for y, row in enumerate(lines):
-    for x, c in enumerate(row):
+for y, row in enumerate(lines, 1):
+    first = None
+    for x, c in enumerate(row, 1):
         if c == " ":
             continue
-        if pos is None:
-            pos = (x, y)
+        if first is None:
+            first = (x, y)
+            if pos is None:
+                pos = first
         grid[(x, y)] = c
-        if y not in minx:
-            minx[y] = x
-        if x not in miny:
-            miny[x] = y
-        maxx[y] = x
-        maxy[x] = y
+        ymins[x] = min(ymins.get(x, 1000000), y)
+        ymaxs[x] = max(ymaxs.get(x, -1000000), y)
+    last = (x, y)
+    warp[(first[0] - 1, first[1])] = last
+    warp[(last[0] + 1, last[1])] = first
+
+for x in ymins:
+    first = (x, ymins[x])
+    last = (x, ymaxs[x])
+    warp[(first[0], first[1] - 1)] = last
+    warp[(last[0], last[1] + 1)] = first
 
 
-moves = {}
+def advance(pos, facing):
+    new_pos = (pos[0] + facing[0], pos[1] + facing[1])
+    new_pos = warp.get(new_pos, new_pos)
+    dest = grid[new_pos]
+    if dest == ".":
+        pos = new_pos
+    elif dest == "#":
+        pass
+    else:
+        assert False, "Unknown dest %s at %s" % (dest, new_pos)
 
-def draw(grid, moves):
-    for y in range(min(miny), max(maxy) + 1):
-        for x in range(min(minx), max(maxx) + 1):
-            c = moves.get((x, y), grid.get((x, y), " "))
-            print(c, end="")
-        print()
-
-
-def move(pos, facing, grid):
-    x, y = pos
-    newx = x + facing[0]
-    newy = y + facing[1]
-    if newx < minx[y]:
-        newx = maxx[y]
-    if newy < miny[x]:
-        newy = maxy[x]
-    if newx > maxx[y]:
-        newx = minx[y]
-    if newy > maxy[x]:
-        newy = miny[x]
-    new_pos = (newx, newy)
-    if grid[new_pos] == ".":
-        pos =  new_pos
     return pos
 
+def rotate(p, facing):
+    offset = {"L": -1, "R": 1}[p]
+    return directions[(directions.index(facing) + offset) % 4]
 
-f =  ">v<^"
-for d in directions:
-    if d.isdigit():
-        for _ in range(int(d)):
-            pos = move(pos, facing, grid)
-            moves[pos] = f[dirs.index(facing)]
+for p in path:
+    if p.isdigit():
+        for _ in range(int(p)):
+            pos = advance(pos, facing)
     else:
-        facing = rotate(facing, d)
-        moves[pos] = f[dirs.index(facing)]
+        facing = rotate(p, facing)
 
-draw(grid, moves)
-col = pos[0] + 1
-row = pos[1] + 1
-
-face_score = dirs.index(facing)
-print(row * 1000 + col * 4 + face_score)
-
-# 30552, low
+print(pos, facing)
+col, row = pos
+print(1000 * row + 4 * col + directions.index(facing))
