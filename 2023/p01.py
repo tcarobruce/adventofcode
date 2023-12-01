4d347ae6aff57e6539bb44a7dc77321531ea754f
Timothy Caro-Bruce <tcarobruce@gmail.com>
2023-12-01 10:45:00 -0800

    1-2 working
    Tricky bit here is matching an overlapping word
    which comes at the end.
    
    Python regex won't find overlaps and will find
    earlier ones before later ones, so for example:
    
    1eighthree
    
    should be '13', but the previous implementation
    would transform the string to '18hree' and return '18'

--- 2023/p01.py
+++ 2023/p01.py
@@ -3,22 +3,27 @@ import re
 
 lines = [ln.strip() for ln in open(sys.argv[1])]
 
+digits = "|".join([str(d) for d in range(1, 10)])
 words = "one|two|three|four|five|six|seven|eight|nine"
-word_map = {w: str(i) for i, w in enumerate(words.split("|"), 1)}
+word_map = {w: i for i, w in enumerate(words.split("|"), 1)}
 
-def replace(m):
-    return word_map[m.group(0)]
+def val(m):
+    if m.isdigit():
+        return int(m)
+    else:
+        return word_map[m]
 
 tot = 0
+patt = re.compile(f"({digits}|{words})")
+revpatt = re.compile(f"({digits}|{words[::-1]})")
 for ln in lines:
     orig = ln
-    ln = re.sub(f"({words})", replace, ln)
-    digs = []
-    for c in ln.strip():
-        if c.isdigit():
-            digs.append(int(c))
-    line_sum = 10 * digs[0] + digs[-1]
-    print(orig, ln, line_sum)
+    first = patt.search(ln).group(0)
+    last = revpatt.search(ln[::-1]).group(0)[::-1]
+    first_val = val(first)
+    last_val = val(last)
+    line_sum = 10 * first_val + last_val
+    print(ln, first, first_val, last, last_val, line_sum)
     tot += line_sum
 
 print(tot)
