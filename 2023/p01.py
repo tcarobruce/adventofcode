b52781ef44befdf3d5f5eae88f79c6abe73edcf1
Timothy Caro-Bruce <tcarobruce@gmail.com>
2023-11-30 21:37:13 -0800

    wrong tree

--- 2023/p01.py
+++ 2023/p01.py
@@ -1,11 +1,22 @@
 import sys
 
+lines = [ln.strip() for ln in open(sys.argv[1])]
+
+words = "zero, one, two, three, four, five, six, seven, eight, nine".split(", ")
+word_map = {w: i for i, w in enumerate(words)}
+
 tot = 0
-for ln in open(sys.argv[1]):
+for ln in lines:
+    orig = ln
+    for w, d in word_map.items():
+        ln = ln.replace(w, str(d))
     digs = []
     for c in ln.strip():
         if c.isdigit():
             digs.append(int(c))
-    tot += 10 * digs[0] + digs[-1]
+    line_sum = 10 * digs[0] + digs[-1]
+    print(orig, ln, line_sum)
+    tot += line_sum
 
 print(tot)
+
