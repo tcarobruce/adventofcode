30c1acef0a4481a9a454d08423f6d57382134603
Timothy Caro-Bruce <tcarobruce@gmail.com>
2023-12-02 08:21:19 -0800

    2-2

--- 2023/p02.py
+++ 2023/p02.py
@@ -1,7 +1,8 @@
 import sys
 import re
+from math import prod
 
-maxs = {
+possible_maxs = {
     "red": 12,
     "green": 13,
     "blue": 14,
@@ -12,13 +13,23 @@ lines = [ln.strip() for ln in open(sys.argv[1])]
 linepatt = re.compile(r"Game (\d+)")
 colorpatt = re.compile(r"([\d]+) (red|green|blue)")
 
-tot = 0
+p1_tot = 0
+p2_tot = 0
 for ln in lines:
     game = int(linepatt.match(ln).group(1))
+    possible = True
+    maxs = {"red": 0, "green": 0, "blue": 0}
     for amount, color in colorpatt.findall(ln):
-        if int(amount) > maxs[color]:
-            break
-    else:
-        tot += game
+        amount = int(amount)
+        if amount > possible_maxs[color]:
+            possible = False
+        maxs[color] = max(maxs[color], amount)
 
-print(tot)
+    power = prod(maxs.values())
+    p2_tot += power
+
+    if possible:
+        p1_tot += game
+
+print(p1_tot)
+print(p2_tot)
