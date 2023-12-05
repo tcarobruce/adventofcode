e5f15c9cb226d6b6e1b6eb1f419303b9524aece1
Timothy Caro-Bruce <tcarobruce@gmail.com>
2023-12-05 13:06:51 -0800

    test assumption about non-overlapping ranges

--- 2023/p05.py
+++ 2023/p05.py
@@ -8,12 +8,11 @@ def get_ints(s):
 
 text = open(sys.argv[1]).read().split("\n\n")
 
-positions = get_ints(text[0])
+seeds = get_ints(text[0])
 
 
 def read_map(chunk):
     lines = chunk.strip().split("\n")
-    print(f"handling {lines[0]}")
     maps = [get_ints(line) for line in lines[1:]]
     return [
         (range(source, source + length), destination - source)
@@ -27,12 +26,28 @@ def find_destination(m, source):
     else:
         return source
 
+def overlap(a, b):
+    return range(max(a.start, b.start), min(a.stop, b.stop))
 
 
-for chunk in text[1:]:
-    m = read_map(chunk)
+def test_overlaps(ranges):
+    for i, a in enumerate(ranges):
+        for b in ranges[i + 1:]:
+            if overlap(a, b):
+                print(f"{a} overlaps with {b}")
+
+
+
+
+maps = [read_map(chunk) for chunk in text[1:]]
+
+for m in maps:
+    test_overlaps([x[0] for x in m])
+
+
+# p1
+positions = seeds
+for m in maps:
     positions = [find_destination(m, p) for p in positions]
 
 print(min(positions))
-
-
