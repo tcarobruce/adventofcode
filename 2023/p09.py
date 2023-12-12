1e7150e2f39bc55b013e1a7bbf470c33631bfcbb
Timothy Caro-Bruce <tcarobruce@gmail.com>
2023-12-11 21:42:52 -0800

    missed commit from earlier day

--- 2023/p09.py
+++ 2023/p09.py
@@ -10,4 +10,15 @@ def find_next(nums):
     return nums[-1] + find_next(diffs)
 
 
+# p1
 print(sum([find_next(line) for line in lines]))
+
+
+def find_prev(nums):
+    if len(set(nums)) == 1:
+        return nums[0]
+    diffs = [(b - a) for a, b in zip(nums[:-1], nums[1:])]
+    return nums[0] - find_prev(diffs)
+
+# p2
+print(sum([find_prev(line) for line in lines]))
