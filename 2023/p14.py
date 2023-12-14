5e13abdb4d316820956d95c3e2a520220de45c58
Timothy Caro-Bruce <tcarobruce@gmail.com>
2023-12-13 21:40:35 -0800

    14-2

--- 2023/p14.py
+++ 2023/p14.py
@@ -5,6 +5,13 @@ fn = sys.argv[1]
 lines = [ln.strip() for ln in open(fn)]
 
 
+def rotate_cw(lines):
+    return [
+        "".join([ln[i] for ln in lines][::-1])
+        for i, _ in enumerate(lines[0])
+    ]
+
+
 def tilt_right(row):
     rounds = 0
     tilted = []
@@ -26,10 +33,30 @@ def load(line):
     return sum([i for i, c in enumerate(line, 1) if c == "O"])
 
 
-tot = 0
-for i, _ in enumerate(lines[0]):
-    ln = tilt_right([ln[i] for ln in lines][::-1])
-    print(ln, load(ln))
-    tot += load(ln)
+# p1
+print(sum([load(tilt_right(ln)) for ln in rotate_cw(lines)]))
 
-print(tot)
+
+def cycle(lines):
+    for _ in range(4):
+        lines = [tilt_right(ln) for ln in rotate_cw(lines)]
+    return lines
+
+
+seen = {}
+cycles = 1000000000
+for i in range(cycles):
+    k = "\n".join(lines)
+    if k in seen:
+        interval = i - seen[k]
+        print(f"repeat at {i}, prev {seen[k]}, interval {i - seen[k]}")
+        break
+    seen[k] = i
+    lines = cycle(lines)
+
+mod = (cycles - i) % interval
+
+for _ in range(mod):
+    lines = cycle(lines)
+
+print(sum([load(ln) for ln in rotate_cw(lines)]))
