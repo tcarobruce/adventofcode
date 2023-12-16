cb30f819e00af841b45c0174ad92bb2d3817dd11
Timothy Caro-Bruce <tcarobruce@gmail.com>
2023-12-15 21:42:24 -0800

    16-2 brute force is enough :shrug:

--- 2023/p16.py
+++ 2023/p16.py
@@ -32,31 +32,44 @@ SPLITTERS = {
     },
 }
 
-beams = [(V(0, 0), V(1, 0))]
 
-energized = set()
-seen = set()
+def get_energized(position, velocity):
+    beams = [(position, velocity)]
+    energized = set()
+    seen = set()
 
-while beams:
-    position, velocity = beams.pop(0)
-    if (position, velocity) in seen:
-        continue
-    if position.els[0] < 0 or position.els[0] >= len(lines[0]):
-        continue
-    if position.els[1] < 0 or position.els[1] >= len(lines):
-        continue
-    seen.add((position, velocity))
-    energized.add(position)
-    c = lines[position.els[1]][position.els[0]]
-    #print(c, position, velocity)
-    if c == ".":
-        beams.append((position + velocity, velocity))
-    elif c in MIRRORS:
-        velocity = MIRRORS[c][velocity]
-        beams.append((position + velocity, velocity))
-    elif c in SPLITTERS:
-        for nv in SPLITTERS[c][velocity]:
-            beams.append((position + nv, nv))
-    #print(beams, len(energized))
+    while beams:
+        position, velocity = beams.pop(0)
+        if (position, velocity) in seen:
+            continue
+        if position.els[0] < 0 or position.els[0] >= len(lines[0]):
+            continue
+        if position.els[1] < 0 or position.els[1] >= len(lines):
+            continue
+        seen.add((position, velocity))
+        energized.add(position)
+        c = lines[position.els[1]][position.els[0]]
+        #print(c, position, velocity)
+        if c == ".":
+            beams.append((position + velocity, velocity))
+        elif c in MIRRORS:
+            velocity = MIRRORS[c][velocity]
+            beams.append((position + velocity, velocity))
+        elif c in SPLITTERS:
+            for nv in SPLITTERS[c][velocity]:
+                beams.append((position + nv, nv))
+    return energized
 
-print(len(energized))
+print(len(get_energized(V(0, 0), V(1, 0))))
+
+
+m = 0
+for x, _ in enumerate(lines[0]):
+    m = max(m, len(get_energized(V(x, 0), V(0, 1))))
+    m = max(m, len(get_energized(V(x, len(lines) - 1), V(0, -1))))
+
+for y, _ in enumerate(lines):
+    m = max(m, len(get_energized(V(0, y), V(1, 0))))
+    m = max(m, len(get_energized(V(len(lines[0]) - 1, y), V(0, -1))))
+
+print(m)
