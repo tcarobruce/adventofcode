94680c50491667900aa879a2a536ffac04146dfc
Timothy Caro-Bruce <tcarobruce@gmail.com>
2023-12-17 19:03:03 -0800

    17-2 copy pasta

--- 2023/p17.py
+++ 2023/p17.py
@@ -37,4 +37,34 @@ def traverse():
                 heappush(q, (heat_loss + hl, np, nv, 1))
 
 
-print(traverse())
+def traverse2():
+    q = [(0, V(0, 0), vel, 0) for vel in [V(1, 0), V(0, 1)]]
+    seen = set()
+
+    while True:
+        print(q[0])
+        heat_loss, pos, velocity, conseq = heappop(q)
+        if pos == dest:
+            return heat_loss
+
+        if (pos, velocity, conseq) in seen:
+            continue
+        seen.add((pos, velocity, conseq))
+
+        if conseq < 10:
+            np = pos + velocity
+            hl = G.get(np)
+            if hl is not None:
+                heappush(q, (heat_loss + hl, np, velocity, conseq + 1))
+
+        if conseq < 4:
+            continue
+        for nv in [velocity.rotate_cw(), velocity.rotate_ccw()]:
+            np = pos + nv
+            hl = G.get(np)
+            if hl is not None:
+                heappush(q, (heat_loss + hl, np, nv, 1))
+
+
+print(traverse1())
+print(traverse2())
