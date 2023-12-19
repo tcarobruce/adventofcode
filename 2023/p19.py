e6902eaa67498cb734f673517c8f9be5b0c47df4
Timothy Caro-Bruce <tcarobruce@gmail.com>
2023-12-18 22:02:31 -0800

    19-2

--- 2023/p19.py
+++ 2023/p19.py
@@ -1,6 +1,7 @@
 import sys
 import re
 from operator import lt, gt
+from math import prod
 
 OPS = {">": gt, "<": lt}
 
@@ -14,7 +15,7 @@ for ln in rules_text.split():
     for rp in rule.split(","):
         if (m := re.match(r"([a-z]+)([<>])(\d+):([a-zAR]+)", rp)):
             category, op, amt, next_label = m.groups()
-            r["subs"].append((category, OPS[op], int(amt), next_label))
+            r["subs"].append((category, op, int(amt), next_label))
         else:
             assert re.match(r"^[a-zRA]+$", rp), "Missed on rp"
             r["else"] = rp
@@ -29,7 +30,7 @@ def is_accepted(part, rules, label="in"):
     while label not in "AR":
         rule = rules[label]
         for category, op, amt, next_label in rule["subs"]:
-            if op(part[category], amt):
+            if OPS[op](part[category], amt):
                 label = next_label
                 break
         else:
@@ -37,7 +38,44 @@ def is_accepted(part, rules, label="in"):
     return label == "A"
 
 
+def intersect(a, b):
+    return range(max(a.start, b.start), min(a.stop, b.stop))
 
+def intersect_multi(rs, b):
+    nrs = []
+    for r in rs:
+        nr = intersect(r, b)
+        if nr:
+            nrs.append(nr)
+    return nrs
+
+
+def ways(rules, ranges=None, label="in"):
+    if ranges is None:
+        ranges = {c: [range(1, 4001)] for c in 'xmas'}
+    if label == "A":
+        print(ranges)
+        return prod([
+            sum([len(r) for r in rs])
+            for rs in ranges.values()
+        ])
+    if label == "R" or not all(ranges.values()):
+        return 0
+
+    rule = rules[label]
+    total = 0
+    for category, op, amt, next_label in rule["subs"]:
+        rs = ranges[category]
+        if op == "<":
+            follow_label_ranges = {**ranges, category: intersect_multi(rs, range(1, amt))}
+            ranges = {**ranges, category: intersect_multi(rs, range(amt, 4001))}
+        else:
+            follow_label_ranges = {**ranges, category: intersect_multi(rs, range(amt + 1, 4001))}
+            ranges = {**ranges, category: intersect_multi(rs, range(1, amt + 1))}
+
+        total += ways(rules, ranges=follow_label_ranges, label=next_label)
+
+    return total + ways(rules, ranges=ranges, label=rule["else"])
 
 # from pprint import pprint
 # pprint(rules)
@@ -45,4 +83,4 @@ def is_accepted(part, rules, label="in"):
 
 
 print(sum([sum(p.values()) for p in parts if is_accepted(p, rules)]))
-
+print(ways(rules))
