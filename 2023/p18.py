7dc57fba48d4eb5594f04c0ecf329ab601f0cae1
Timothy Caro-Bruce <tcarobruce@gmail.com>
2023-12-18 17:32:31 -0800

    18-2

--- 2023/p18.py
+++ 2023/p18.py
@@ -1,52 +1,64 @@
 import sys
 import re
 from util import Vec as V
+from bisect import insort_left, bisect
+from itertools import groupby
+from collections import defaultdict
 
-lines = [ln.strip() for ln in open(sys.argv[1])]
-p = re.compile(r"([RLDU]) (\d+) \(#([a-f0-9]{6})\)")
+patt = re.compile(r"([RLDU]) (\d+) \(#([a-f0-9]{6})\)")
+lines = [patt.match(ln.strip()).groups() for ln in open(sys.argv[1])]
+dirs = [
+    ("R", V(1, 0)),
+    ("D", V(0, 1)),
+    ("L", V(-1, 0)),
+    ("U", V(0, -1)),
+]
+dirs_dict = dict(dirs)
+dig_plan = [
+    (dirs_dict[direction], int(count))
+    for direction, count, _ in lines
+]
+dig_plan2 = [
+    (dirs[int(color[-1])][1], int(color[:5], 16))
+    for _, _, color in lines
+]
 
-
-dirs = {
-    "R": V(1, 0),
-    "L": V(-1, 0),
-    "U": V(0, -1),
-    "D": V(0, 1),
-}
-
-def outline(lines):
+def find_horizontal_segments(plan):
+    segs = []
     pos = V(0, 0)
-    G = {pos: None}
-    for line in lines:
-        direction, count, color = p.match(line).groups()
-        v = dirs[direction]
-        for _ in range(int(count)):
-            pos = pos + v
-            G[pos] = color
-    return G
+    lines = defaultdict(list)
+    for direction, count in plan:
+        if direction.els[0] == 0:
+            lines[direction].append(count)
+            pos += direction * count
+        else:
+            lines[direction].append(count)
+            next_pos = pos + (direction * count)
+            #r = direction == V(1, 0)
+            insort_left(segs, (min(pos.els[0], next_pos.els[0]), pos.els[1], True))
+            insort_left(segs, (max(pos.els[0], next_pos.els[0]), pos.els[1], False))
+            pos = next_pos
+    return segs, lines
 
 
-def find_fill(G):
-    mins, maxs = V.extent(G.keys())
-    for x in range(mins.els[0], maxs.els[0]):
-        for y in range(mins.els[1], maxs.els[1]):
-            start = p = V(x, y)
-            if start in G:
-                continue
-            F = dict(G)
-            q = [p]
-            outside = False
-            while q:
-                p = q.pop(0)
-                if not p.in_extent(mins, maxs):
-                    break
-                if p in F:
-                    continue
-                F[p] = 1
-                q.extend([p + v for v in dirs.values()])
+def sum_segments(segments, lines):
+    assert len(segments) % 2 == 0
+    open_spans = []
+    total = 0
+    lastx = None
+    for x, g in groupby(segments, lambda r: r[0]):
+        for start, stop in zip(open_spans[::2], open_spans[1::2]):
+            area = (x - lastx) * (stop - start)
+            #print(lastx, x, start, stop, area)
+            total += area
+        for _, y, starting in g:
+            if starting:
+                insort_left(open_spans, y)
             else:
-                print(start)
-                return len(F)
+                i = bisect(open_spans, y)
+                open_spans = open_spans[:i - 1] + open_spans[i:]
+        lastx = x
+    return total + sum(lines[V(1, 0)]) + sum(lines[V(0, 1)]) + 1
 
-
-print(find_fill(outline(lines)))
-
+print(sum_segments(*find_horizontal_segments(dig_plan)))
+print(sum_segments(*find_horizontal_segments(dig_plan2)))
