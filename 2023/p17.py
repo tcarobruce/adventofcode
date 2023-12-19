68abdc4272973f62ed9d34c870a7b9e5c4e53677
Timothy Caro-Bruce <tcarobruce@gmail.com>
2023-12-18 18:55:27 -0800

    cleanup + optimize

--- 2023/p17.py
+++ 2023/p17.py
@@ -10,60 +10,31 @@ for y, ln in enumerate(open(sys.argv[1])):
 dest = V(x, y)
 
 
-def traverse():
-    q = [(0, V(0, 0), vel, 0) for vel in [V(1, 0), V(0, 1)]]
+def traverse(low, high):
+    q = [(0, V(0, 0), vel) for vel in [V(1, 0), V(0, 1)]]
     seen = set()
 
     while True:
-        print(q[0])
-        heat_loss, pos, velocity, conseq = heappop(q)
+        #print(q)
+        heat_loss, pos, velocity = heappop(q)
         if pos == dest:
             return heat_loss
 
-        if (pos, velocity, conseq) in seen:
+        if (pos, velocity) in seen:
             continue
-        seen.add((pos, velocity, conseq))
+        seen.add((pos, velocity))
 
-        if conseq < 3:
-            np = pos + velocity
-            hl = G.get(np)
-            if hl is not None:
-                heappush(q, (heat_loss + hl, np, velocity, conseq + 1))
+        for v in [velocity.rotate_cw(), velocity.rotate_ccw()]:
+            hlt = heat_loss
+            np = pos
+            for i in range(1, high):
+                np += v
+                hl = G.get(np)
+                if hl is None:
+                    break
+                hlt += hl
+                if i >= low:
+                    heappush(q, (hlt, np, v))
 
-        for nv in [velocity.rotate_cw(), velocity.rotate_ccw()]:
-            np = pos + nv
-            hl = G.get(np)
-            if hl is not None:
-                heappush(q, (heat_loss + hl, np, nv, 1))
-
-
-def traverse2():
-    q = [(0, V(0, 0), vel, 0) for vel in [V(1, 0), V(0, 1)]]
-    seen = set()
-
-    while True:
-        heat_loss, pos, velocity, conseq = heappop(q)
-        if pos == dest:
-            return heat_loss
-
-        if (pos, velocity, conseq) in seen:
-            continue
-        seen.add((pos, velocity, conseq))
-
-        if conseq < 10:
-            np = pos + velocity
-            hl = G.get(np)
-            if hl is not None:
-                heappush(q, (heat_loss + hl, np, velocity, conseq + 1))
-
-        if conseq < 4:
-            continue
-        for nv in [velocity.rotate_cw(), velocity.rotate_ccw()]:
-            np = pos + nv
-            hl = G.get(np)
-            if hl is not None:
-                heappush(q, (heat_loss + hl, np, nv, 1))
-
-
-print(traverse1())
-print(traverse2())
+print(traverse(1, 4))
+print(traverse(4, 11))
