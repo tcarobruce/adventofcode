ee4e30a4c23cd0a38f9a597950475fcc4cc2bbe8
Timothy Caro-Bruce <tcarobruce@gmail.com>
2023-12-19 22:11:48 -0800

    20-1 not working

---
+++ 2023/p20.py
@@ -0,0 +1,49 @@
+import sys
+from collections import deque, Counter, defaultdict
+
+L, H = "low", "high"
+
+lines = [ln.strip() for ln in open(sys.argv[1])]
+M = {}
+for line in lines:
+    module, dests = line.split(" -> ")
+    if module == "broadcaster":
+        type = "broadcaster"
+    else:
+        type, module = module[0], module[1:]
+    dests = dests.split(", ")
+    for dest in dests:
+        M.setdefault(dest, ("output", []))
+    M[module] = (type, dests)
+
+STATE = {m: False for m in M if M[m][0] == "%"}
+MEM = {m: defaultdict(lambda: L) for m in M if M[m][0] == "&"}
+
+def process_pulse():
+    q = deque([("broadcaster", L, "button")])
+    sent = Counter()
+    while q:
+        module, pulse, sender = q.popleft()
+        sent[pulse] += 1
+        type, dests = M[module]
+        if type == "broadcaster":
+            pass
+        elif type == "%":
+            if pulse == H:
+                continue
+            pulse = [H, L][STATE[module]]
+            STATE[module] = not STATE[module]
+        elif type == "&":
+            MEM[module][sender] = pulse
+            pulse = [H, L][all([v == H for v in MEM[module].values()])]
+        for dest in dests:
+            q.append((dest, pulse, module))
+    return sent
+
+total = Counter()
+for _ in range(1000):
+    total += process_pulse()
+
+# 725407118 too high
+
+print(total, total[L] * total[H])
