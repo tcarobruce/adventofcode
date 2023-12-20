9115b0434b3d8ad9d0a86b975d2c1bad85b12d19
Timothy Caro-Bruce <tcarobruce@gmail.com>
2023-12-20 12:58:33 -0800

    20-1 working, hacky

--- 2023/p20.py
+++ 2023/p20.py
@@ -1,7 +1,10 @@
 import sys
+import os
 from collections import deque, Counter, defaultdict
+from itertools import count
+from util import lcm
 
-L, H = "low", "high"
+L, H = "L", "H"
 
 lines = [ln.strip() for ln in open(sys.argv[1])]
 M = {}
@@ -18,15 +21,26 @@ for line in lines:
         MEM.setdefault(dest, {})[module] = L
     M[module] = (type, dests)
 
+MEM = {k: v for k, v in MEM.items() if M[k][0] == "&"}
 STATE = {m: False for m in M if M[m][0] == "%"}
 
-def process_pulse():
+intervals = {}
+lasts = {}
+
+def process_pulse(i):
     q = deque([("broadcaster", L, "button")])
-    sent = Counter()
     while q:
         module, pulse, sender = q.popleft()
         #print(f"{sender} -{pulse}-> {module}")
-        sent[pulse] += 1
+        if module in ("hn", "kh", "lz", "tg") and pulse == L:
+            if module in lasts:
+                intervals.setdefault(module, set()).add(i - lasts[module])
+                print(f"{module} {intervals[module]} at {i}")
+            lasts[module] = i
+            if len(intervals) == 4:
+                return True
+        if module == "rx" and pulse == L:
+            return True
         type, dests = M[module]
         if type == "broadcaster":
             pass
@@ -40,11 +54,32 @@ def process_pulse():
             pulse = [H, L][all([v == H for v in MEM[module].values()])]
         for dest in dests:
             q.append((dest, pulse, module))
-    return sent
 
-total = Counter()
+# total = Counter()
 
-for _ in range(1000):
-    total += process_pulse()
+# for _ in range(1000):
+#     total += process_pulse()
 
-print(total, total[L] * total[H])
+# print(total, total[L] * total[H])
+
+def pmem():
+    for k, v in sorted(MEM.items()):
+        if not isinstance(v, dict):
+            continue
+        print(k, end="   ")
+        for f, lh in sorted(v.items()):
+            print(f"{f}-{lh}", end=" ")
+        print()
+
+for i in count(1):
+    # os.system("clear")
+    # print(i)
+    # pmem()
+    # print()
+    # if i % 1000 == 0:
+    #     print(i)
+    if process_pulse(i):
+        print(i)
+        break
+
+print(lcm(*[s.pop() for s in intervals.values()]))
