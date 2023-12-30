1520bc7c1979fbaefe46c4ec2358afaf6222f7e1
Timothy Caro-Bruce <tcarobruce@gmail.com>
2023-12-30 11:42:19 -0800

    drawing to see patterns

--- 2023/p21.py
+++ 2023/p21.py
@@ -1,6 +1,7 @@
 import sys
 from util import Vec as V
 from collections import deque
+from PIL import Image, ImageDraw, ImageFont
 
 
 G = {}
@@ -14,23 +15,76 @@ for y, ln in enumerate(open(sys.argv[1])):
             c = "."
         G[v] = c
 
+SIZE = v + V(1, 1)
 
-def walk(start, max_steps):
+def get_grid(v):
+    return G.get(V(v.els[0] % SIZE.els[0], v.els[1] % SIZE.els[1]))
+
+def draw(ext, reached):
+    for y in range(ext[0].els[1], ext[1].els[1] + 1):
+        for x in range(ext[0].els[0], ext[1].els[0] + 1):
+            v = V(x, y)
+            if v in reached:
+                c = "O"
+            else:
+                c = get_grid(v)
+                if c == '.':
+                    c = ' '
+            print(c, end="")
+        print()
+
+
+def draw(ext, reached, steps):
+    img = Image.new('RGB', (ext[1] - ext[0] + V(1, 1)).els)
+    for y in range(ext[0].els[1], ext[1].els[1] + 1):
+        for x in range(ext[0].els[0], ext[1].els[0] + 1):
+            v = V(x, y)
+            c = None
+            if v in reached:
+                c = (255, 127, 0)
+            else:
+                if get_grid(v) == '.':
+                    c = (255, 255, 255)
+            if c is not None:
+                xy = (v - ext[0]).els
+                img.putpixel(xy, c)
+    I1 = ImageDraw.Draw(img)
+    font = ImageFont.truetype('/usr/share/fonts/truetype/ubuntu/UbuntuMono-B.ttf', 24)
+    I1.text((10, 10), str(steps), fill=(0, 120, 200), font=font)
+    img.show()
+
+
+def walk(start):
     q = deque([(0, start)])
     reached = [set(), set()]
 
+    last = 0
     while q:
         #print(q, len(right_dist))
         steps, pos = q.popleft()
-        if G.get(pos) != ".":
+        if steps != last:
+            yield last, reached[last % 2]
+            last = steps
+        if get_grid(pos) != ".":
             continue
-        mod = (max_steps - steps) % 2
+        mod = steps % 2
         if pos in reached[mod]:
             continue
         reached[mod].add(pos)
-        if steps < max_steps:
-            q.extend([(steps + 1, p) for p in pos.neighbors()])
-    return len(reached[0])
+        q.extend([(steps + 1, p) for p in pos.neighbors()])
 
-print(walk(S, 6))
-print(walk(S, 64))
+
+s = 400
+ext = S - V(s, s), S + V(s, s)
+from os import system
+results = {}
+for steps, r in walk(S):
+    l = len(r)
+    results[steps] = l
+    if steps and (steps - 65) % 262 == 0:
+        #draw(ext, r, steps)
+        d = (steps - 65) // 131
+        a, b = V.extent(r)
+        a -= V(65, 65)
+        b -= V(65, 65)
+        print(d, steps, l, b.els[0] - a.els[0])
