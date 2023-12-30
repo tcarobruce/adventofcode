eaf329ffd2fa8ba923f7a1370c515069228167e4
Timothy Caro-Bruce <tcarobruce@gmail.com>
2023-12-29 16:06:04 -0800

    22-2, too low

--- 2023/p22.py
+++ 2023/p22.py
@@ -2,6 +2,7 @@ import sys
 from util import readints, Vec as V
 from string import ascii_uppercase
 from collections import Counter
+from functools import cache
 
 lines = [ln.strip() for ln in open(sys.argv[1])]
 
@@ -51,25 +52,45 @@ def drop_many(bricks):
     by_height = sorted(bricks, key=lambda b: bricks[b][0].els[2])
     for brick in by_height:
         drop(terrain, dependencies, brick)
-        #print(brick, BRICKS[brick])
-        #print(terrain)
-        #print(dependencies)
-        #print()
 
     return terrain, dependencies
 
 
 terrain, dependencies = drop_many(BRICKS)
+
 disintegratable = set(BRICKS)
 for deps in dependencies.values():
     if len(deps) == 1:
         disintegratable.discard(deps[0])
 
-print(disintegratable)
 print(len(disintegratable))
-# c = Counter()
-# for deps in dependencies.values():
-#     c.update(deps)
 
-# print(len([b for b in bricks if c[b] <= 1]
+supports = {}
+for depender, supporters in dependencies.items():
+    for supporter in supporters:
+        supports.setdefault(supporter, []).append(depender)
 
+print(dependencies)
+print(supports)
+
+@cache
+def _chain_reaction(brick, falling_t=None):
+    if falling_t is None:
+        falling_t = ()
+    falling = set(falling_t) | {brick}
+    new_falling = set()
+    for dep in supports.get(brick, []):
+        if not set(dependencies[dep]) - falling:
+            new_falling.add(dep)
+    if new_falling:
+        falling.update(new_falling)
+        falling_t = tuple(sorted(falling))
+        for dep in new_falling:
+            falling.update(_chain_reaction(dep, falling_t=falling_t))
+    return falling
+
+def chain_reaction_length(brick):
+    return len(_chain_reaction(brick) - {brick})
+
+
+print(sum([chain_reaction_length(b) for b in BRICKS]))
