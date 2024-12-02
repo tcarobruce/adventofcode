8cb53b2809a9dd0115fe6a078600fd5177079f17
Timothy Caro-Bruce <tcarobruce@gmail.com>
2024-12-01 20:30:56 -0800

    1-2

--- 2024/p01.py
+++ 2024/p01.py
@@ -1,6 +1,11 @@
 import sys
+from collections import Counter
 from util import readints
 
 lines = [readints(ln) for ln in open(sys.argv[1])]
 left, right = zip(*lines)
+
 print(sum([abs(l - r) for l, r in zip(sorted(left), sorted(right))]))  # part 1
+
+count_right = Counter(right)
+print(sum([l * count_right[l] for l in left]))  # part 2
