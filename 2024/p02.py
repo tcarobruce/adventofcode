6b74825518c82eabe236520d8d307ce3fc8d9f4a
Timothy Caro-Bruce <tcarobruce@gmail.com>
2024-12-02 16:51:30 -0800

    2-2

--- 2024/p02.py
+++ 2024/p02.py
@@ -14,3 +14,11 @@ def is_safe(nums):
     return _is_safe(nums) or _is_safe(nums[::-1])
 
 print(len([ln for ln in lines if is_safe(ln)]))
+
+def is_sorta_safe(nums):
+    return (
+        is_safe(nums) or
+        any(is_safe(nums[:i] + nums[i+1:]) for i in range(len(nums)))
+    )
+
+print(len([ln for ln in lines if is_sorta_safe(ln)]))
