1418a0d4391b6e21cc1382d759cf27a588b4d3a3
Timothy Caro-Bruce <tcarobruce@gmail.com>
2024-12-06 09:25:23 -0800

    6-2

--- 2024/p06.py
+++ 2024/p06.py
@@ -9,29 +9,49 @@ by_coord = (
     for x, c in enumerate(ln.strip())
 )
 grid = {}
-start = None
-direction = V(0, -1)
+START = None
+DIRECTION = V(0, -1)
 
 for (x, y), c in by_coord:
-    grid[V(x, y)] = c
     if c == '^':  # only this char is used for starting position
-        start = V(x, y)
+        START = V(x, y)
+        c = '.'
+    grid[V(x, y)] = c
 
 
 def walk(start, direction):
-    seen = {start}
+    seen = {(start, direction)}
     position = start
     while True:
         next_position = position + direction
         c = grid.get(next_position)
-        if c is None:
-            return seen
+        if (next_position, direction) in seen:
+            return seen, True
+        elif c is None:
+            return seen, False
         elif c == '#':
             direction = direction.rotate_cw()
-        else:
+        elif c == '.':
             position = next_position
-            seen.add(next_position)
+            seen.add((next_position, direction))
+        else:
+            raise Exception("Bad place %s at %s!" % (next_position, c))
 
-visited = walk(start, direction)
-print(visited)
-print(len(visited))
+visited, loop = walk(START, DIRECTION)
+positions = set([v[0] for v in visited])
+ct = len(positions)
+print(ct)
+
+tot = 0
+for i, pos in enumerate(positions, 1):
+    print(f"{i}/{ct}")
+    assert grid[pos] == '.'
+    grid[pos] = '#'
+    _, loop = walk(START, DIRECTION)
+    if loop:
+        tot += 1
+    grid[pos] = '.'
+
+print(tot)
+
+
