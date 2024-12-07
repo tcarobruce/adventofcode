43094d0a825ebbfdb8987b0e73f5f41ab93f6046
Timothy Caro-Bruce <tcarobruce@gmail.com>
2024-12-07 09:32:55 -0800

    cleanup

--- 2024/p07.py
+++ 2024/p07.py
@@ -2,29 +2,17 @@ import sys
 from util import readints
 lines = [readints(ln) for ln in open(sys.argv[1])]
 
-def test_line(line):
-    goal = line[0]
-    vals = [line[1]]
-    for o in line[2:]:
-        vals = [v + o for v in vals] + [v * o for v in vals]
-    return goal in vals
-
-def test_line2(line):
-    goal = line[0]
-    vals = [line[1]]
-    for o in line[2:]:
-        vals = [v + o for v in vals] + [v * o for v in vals] + [concat(v, o) for v in vals]
-    return goal in vals
-
-
 def concat(a, b):
     return int(str(a) + str(b))
 
-tot = 0
-tot2 = 0
-for ln in lines:
-    t = test_line2(ln)
-    print(ln, t)
-    if t:
-        tot += ln[0]
-print(tot)
+def test_line(line):
+    goal = line[0]
+    p1 = p2 = [line[1]]
+    for o in line[2:]:
+        p1 = [v + o for v in p1] + [v * o for v in p1]
+        p2 = [v + o for v in p2] + [v * o for v in p2] + [concat(v, o) for v in p2]
+    return (goal * (goal in p1), goal * (goal in p2))
+
+tests = [test_line(ln) for ln in lines]
+for p in zip(*tests):
+    print(sum(p))
