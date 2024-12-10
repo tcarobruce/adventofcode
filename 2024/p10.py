3bb697beee4f49ea85e20468322687dac4bf800a
Timothy Caro-Bruce <tcarobruce@gmail.com>
2024-12-09 21:17:50 -0800

    10-2

--- 2024/p10.py
+++ 2024/p10.py
@@ -8,8 +8,6 @@ trailheads = []
 for y, ln in enumerate(open(sys.argv[1])):
     for x, c in enumerate(ln.strip()):
         coord = V(x, y)
-        if c == '.':
-            c = -1
         grid[coord] = int(c)
         if int(c) == 0:
             trailheads.append(coord)
@@ -25,5 +23,16 @@ def find_trails(pos, val=0):
             total |= find_trails(v, val=val+1)
     return total
 
+@cache
+def find_trails2(pos, val=0):
+    if val == 9:
+        return 1
+    total = 0
+    for v in pos.neighbors():
+        if grid.get(v) == val + 1:
+            total += find_trails2(v, val=val+1)
+    return total
+
 
 print(sum([len(find_trails(th)) for th in trailheads]))
+print(sum([find_trails2(th) for th in trailheads]))
