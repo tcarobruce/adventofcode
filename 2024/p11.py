7b4f0e0b69563745f5d97d57cceb0c21e79efe2a
Timothy Caro-Bruce <tcarobruce@gmail.com>
2024-12-10 21:22:48 -0800

    11-2, rewrite with memoization

--- 2024/p11.py
+++ 2024/p11.py
@@ -1,25 +1,24 @@
 import sys
 from util import readints
+from functools import cache
 
 stones = readints(open(sys.argv[1]).read())
 
-def blink(stones):
-    r = []
-    for s in stones:
-        if s == 0:
-            r.append(1)
-        elif len(ss := str(s)) % 2 == 0:
-            m = len(ss) // 2
-            r.append(int(ss[:m]))
-            r.append(int(ss[m:]))
-        else:
-            r.append(s * 2024)
-    return r
+@cache
+def stones_after(n, i):
+    if i == 0:
+        return 1
+    elif n == 0:
+        return stones_after(1, i - 1)
+    elif (sl := len(ss := str(n))) % 2 == 0:
+        m = sl // 2
+        return (
+            stones_after(int(ss[:m]), i - 1) +
+            stones_after(int(ss[m:]), i - 1)
+        )
+    else:
+        return stones_after(n * 2024, i - 1)
 
 
-for i in range(1, 26):
-    r = blink(stones)
-    print(i, len(r))
-    stones = r
-
-
+print(sum([stones_after(n, 25) for n in stones]))
+print(sum([stones_after(n, 75) for n in stones]))
