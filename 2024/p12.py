feaf3ced9689981e5a22f4cc34891bd39e41515f
Timothy Caro-Bruce <tcarobruce@gmail.com>
2024-12-11 21:57:46 -0800

    consolidate

--- 2024/p12.py
+++ 2024/p12.py
@@ -2,46 +2,15 @@ import sys
 from util import readgridv, Vec as V
 from collections import deque
 
+dirs = [V(1, 0), V(0, 1), V(-1, 0), V(0, -1)]
 grid = readgridv(open(sys.argv[1]))
 
 def read_region(start):
     c = grid.get(start)
     q = deque([start])
     seen = {start}
-    fences = 0
-    area = 0
-    while q:
-        pos = q.popleft()
-        area += 1
-        for n in pos.neighbors():
-            if n in seen:
-                continue
-            if grid.get(n) == c:
-                seen.add(n)
-                q.append(n)
-            else:
-                fences += 1
-    return area, fences, seen
-
-all_seen = set()
-
-total = 0
-for g in grid:
-    if g in all_seen:
-        continue
-    area, fences, seen = read_region(g)
-    total += area * fences
-    all_seen |= seen
-
-print(total)
-
-dirs = [V(1, 0), V(0, 1), V(-1, 0), V(0, -1)]
-
-def read_region2(start):
-    c = grid.get(start)
-    q = deque([start])
-    seen = {start}
-    fences = 0
+    fence_segments = 0
+    fence_lines = 0
     area = 0
     while q:
         pos = q.popleft()
@@ -54,21 +23,22 @@ def read_region2(start):
                 seen.add(n)
                 q.append(n)
                 continue
-
+            fence_segments += 1
             rot = dirs[(i + 1) % 4]
             if grid.get(pos + rot) != c or grid.get(pos + rot + d) == c:
-                fences += 1
-
-    return area, fences, seen
+                fence_lines += 1
+    return area, fence_segments, fence_lines, seen
 
 all_seen = set()
 
-total = 0
+p1 = p2 = 0
 for g in grid:
     if g in all_seen:
         continue
-    area, fences, seen = read_region2(g)
-    total += area * fences
+    area, fence_segments, fence_lines, seen = read_region(g)
+    p1 += area * fence_segments
+    p2 += area * fence_lines
     all_seen |= seen
 
-print(total)
+print(p1)
+print(p2)
