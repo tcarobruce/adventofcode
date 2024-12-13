2878fcc53bece945e2a9ca843cbb5b8e783a3c9d
Timothy Caro-Bruce <tcarobruce@gmail.com>
2024-12-13 12:10:26 -0800

    13-2 ugh remembering linear algebra

--- 2024/p13.py
+++ 2024/p13.py
@@ -1,7 +1,7 @@
 import sys
 import re
 
-from util import readints, readgrid, Vec as V
+from util import readints, Vec as V
 
 '''
 Button A: X+94, Y+34
@@ -9,36 +9,27 @@ Button B: X+22, Y+67
 Prize: X=8400, Y=5400
 '''
 
-
 machine_txts = open(sys.argv[1]).read().strip().split('\n\n')
 machines = []
 for machine_txt in machine_txts:
     machines.append([readints(ln) for ln in machine_txt.split('\n')])
 
-def min_price(machine):
-    a_price = 3
-    b_price = 1
-    a = V(*machine[0])
-    b = V(*machine[1])
-    prize = V(*machine[2])
+def solve_machine(machine):
+    xs, ys = zip(*machine)
+    orig = xs, ys
+    xs, ys = [xi * ys[0] for xi in xs], [yi * xs[0] for yi in ys]
+    assert xs[0] == ys[0]
+    b, br = divmod((xs[2] - ys[2]), (xs[1] - ys[1]))
+    a, ar = divmod(xs[2] - (b * xs[1]), xs[0])
+    if ar != 0 or br != 0:
+        # hmm I don't get why one would be 0 but not the other, but this works
+        return 0
+    return 3 * a + b
 
-    m = None
 
-    for ai in range(prize.els[0] // a.els[0] + 1):
-        claw = a * ai
-        bi = (prize.els[0] - claw.els[0]) // b.els[0]
-        claw += b * bi
-        #print(ai, bi, claw.els)
-        if claw == prize:
-            cost = 3 * ai + bi
-            if m is None or cost < m:
-                m = cost
-    return m
-
-total = 0
-for machine in machines:
-    m = min_price(machine)
-    if m is not None:
-        total += m
-
-print(total)
+print(sum([solve_machine(m) for m in machines]))
+inc = 10000000000000
+big_machines = [
+    m[:2] + [[p + inc for p in m[2]]] for m in machines
+]
+print(sum([solve_machine(m) for m in big_machines]))
