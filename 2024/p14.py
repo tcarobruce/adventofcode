7b8a07c10beaf8b44fb9754e503d6244f9f88b67
Timothy Caro-Bruce <tcarobruce@gmail.com>
2024-12-14 20:43:18 -0800

    14-2, cheese

--- 2024/p14.py
+++ 2024/p14.py
@@ -2,6 +2,8 @@ import sys
 
 from util import Vec as V, readints
 from math import prod
+from time import sleep
+from subprocess import run
 
 lines = [readints(ln) for ln in open(sys.argv[1])]
 
@@ -13,10 +15,6 @@ robots = [[V(*r[:2]), V(*r[2:])] for r in lines]
 def move(r):
     r[0] += r[1]
     r[0] = r[0].wrap(DIMS)
-
-for _ in range(100):
-    for r in robots:
-        move(r)
 
 
 def quad(pos):
@@ -33,12 +31,42 @@ def quad(pos):
         elif y > HEIGHT // 2:
             return 3
 
-quads = [0] * 4
-for pos, _ in robots:
-    q = quad(pos)
-    if q is not None:
-        quads[q] += 1
+def calculate_quads(robots):
+    quads = [0] * 4
+    for pos, _ in robots:
+        q = quad(pos)
+        if q is not None:
+            quads[q] += 1
 
-print(quads)
-print(prod(quads))
+    return prod(quads)
 
+def draw(robots):
+    positions = set([r[0].els for r in robots])
+    img = []
+    is_it = False
+    for y in range(HEIGHT):
+        row = ""
+        for x in range(WIDTH):
+            row += ("X" if (x, y) in positions else ".")
+        img.append(row)
+        if "XXXXXXXXXXXXX" in row:
+            is_it = True
+    return img, is_it
+
+
+i = 0
+quads = None
+while True:
+    i += 1
+    print(i)
+    for r in robots:
+        move(r)
+    img, is_it = draw(robots)
+    if i == 100:
+        quads = calculate_quads(robots))
+    if is_it:
+        print("\n".join(img))
+        break
+
+print("QUADS:", quads)
+print(i)
