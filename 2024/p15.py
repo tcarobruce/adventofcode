b6015dfa05bc757121d9c0774bf496f38ccccdba
Timothy Caro-Bruce <tcarobruce@gmail.com>
2024-12-14 21:58:31 -0800

    15-2

--- 2024/p15.py
+++ 2024/p15.py
@@ -46,3 +46,82 @@ for v, c in grid.items():
     if c == "O":
         total += v.els[0] + 100 * v.els[1]
 print(total)
+
+
+grid = {}
+for y, ln in enumerate(warehouse.split("\n")):
+    for x, c in enumerate(ln):
+        x *= 2
+        v1 = V(x, y)
+        v2 = V(x + 1, y)
+        if c in "#.":
+            grid[v1] = grid[v2] = c
+        elif c == "@":
+            grid[v1] = "@"
+            robot = v1
+            grid[v2] = "."
+        elif c == "O":
+            grid[v1] = "["
+            grid[v2] = "]"
+
+def can_move(pos, d):
+    c = grid.get(pos + d)
+    if c == ".":
+        return set([pos])
+    if c == "#":
+        return None
+    if c in "[]":
+        one = can_move(pos + d, d)
+        if d.els[0]:
+            if one is not None:
+                return set([pos]) | one
+        else:
+            flip = {"[": V(1, 0), "]": V(-1, 0)}
+            other = can_move(pos + d + flip[c], d)
+            if one is not None and other is not None:
+                return set([pos]) | one | other
+
+def shift2(start, d):
+    pos = start
+    moves = can_move(pos, d)
+    if moves is None:
+        return start
+    to_move = {}
+    for m in moves:
+        to_move[m] = grid.pop(m)
+        grid[m] = '.'
+    for m in moves:
+        grid[m + d] = to_move[m]
+    grid[start] = '.'
+    return start + d
+
+
+maxx = max([v.els[0] for v in grid])
+maxy = max([v.els[1] for v in grid])
+
+def draw():
+    for y in range(maxy + 1):
+        for x in range(maxx + 1):
+            print(grid[V(x, y)], end="")
+        print()
+
+
+
+#print("Initial state:")
+#draw()
+for i in instructions:
+    d = dirs[i]
+    robot = shift2(robot, d)
+    #print()
+    #print(f"Move {i}:")
+    #draw()
+    #input()
+
+
+total = 0
+for v, c in grid.items():
+    if c == "[":
+        total += v.els[0] + 100 * v.els[1]
+print(total)
+
+
