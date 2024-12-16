30417c36188c3673508ed7c8563b3ff3f24dff38
Timothy Caro-Bruce <tcarobruce@gmail.com>
2024-12-16 13:44:02 -0800

    16-2

--- 2024/p16.py
+++ 2024/p16.py
@@ -4,80 +4,53 @@ from heapq import heappop, heappush
 
 grid = readgridv(open(sys.argv[1]).readlines())
 
-
-dirs = [V(1, 0), V(0, 1), V(-1, 0), V(0, -1)]
-facing = ">v<^"
-
-
-def draw(grid, q, seen):
-    maxx = max([v.els[0] for v in grid])
-    maxy = max([v.els[1] for v in grid])
-    edge_score, edge, _ = q[0]
-    edges = {}
-    seen = {t[0] for t in seen}
-    for t in q:
-        if t[0] == edge_score:
-            edges[t[1]] = t[2]
-        else:
-            break
-
-    for y in range(maxy + 1):
-        for x in range(maxx + 1):
-            v = V(x, y)
-            if v in edges:
-                c = facing[dirs.index(edges[v])]
-            elif v in seen:
-                c = ' '
-            else:
-                c = grid.get(v)
-            print(c, end="")
-        print()
-
-
-def best_path(grid):
-    start = [pos for pos, c in grid.items() if c == "S"][0]
+def best_paths(grid):
     score = 0
+    start = [pos for pos, c in grid.items() if c == "S"][0]
     facing = V(1, 0)
-    seen = {(start, facing): 0}
-    q = [(score, start, facing)]
 
-    while True:
-        score, pos, facing = heappop(q)
+    q = [(score, start, facing, [start])]
+    seen = {(start, facing): [score, set([start])]}
+    winning_score = None
+    winning_nodes = set()
+
+    while q:
+        score, pos, facing, path = heappop(q)
         if grid.get(pos) == "E":
-            return score
-        if grid.get(pos + facing) == 'E':
-            return score + 1
+            if winning_score is None:
+                winning_score = score
+            elif score > winning_score:
+                break
+            winning_nodes.update(path)
+            continue
 
-        if grid.get(pos + facing) in '.':
-            k = (pos + facing, facing)
-            new_score = score + 1
-            seen_k = seen.get(k)
-            if seen_k is None or seen_k > new_score:
-                heappush(q, (new_score, pos + facing, facing))
-                seen[k] = new_score
+        t = seen.get((pos, facing))
+        if t is not None:
+            seen_score, seen_set = t
+            if seen_score < score:
+                continue
+            elif seen_score == score:
+                seen_set.update(path)
+            else:
+                seen[(pos, facing)] = score, set(path)
+        else:
+            seen[(pos, facing)] = score, set(path)
 
-        ccw = dirs[(dirs.index(facing) - 1) % 4]
-        cw = dirs[(dirs.index(facing) + 1) % 4]
+        n = pos + facing
+        if grid.get(n) in '.E':
+            heappush(q, (score + 1, n, facing, path + [n]))
 
-        new_score = score + 1000
+        cw = facing.rotate_cw()
+        if grid.get(pos + cw) in '.E':
+            heappush(q, (score + 1000, pos, cw, path))
 
-        k = (pos, ccw)
-        seen_k = seen.get(k)
-        if seen_k is None or seen_k > new_score and grid.get(pos + ccw) != "#":
-            heappush(q, (new_score, pos, ccw))
-            seen[k] = new_score
+        ccw = facing.rotate_ccw()
+        if grid.get(pos + ccw) in '.E':
+            heappush(q, (score + 1000, pos, ccw, path))
 
-        k = (pos, cw)
-        seen_k = seen.get(k)
-        if seen_k is None or seen_k > new_score and grid.get(pos + cw) != "#":
-            heappush(q, (score + 1000, pos, cw))
-            seen[k] = new_score
-
-        if 0:
-            draw(grid, q, seen)
-            print(q[0][0])
-            input()
-
+    return winning_score, winning_nodes
 
 
-print(best_path(grid))
+score, nodes = best_paths(grid)
+print(score)
+print(len(nodes))
