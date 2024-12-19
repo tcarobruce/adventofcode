bcd1d34fc6c5549f2182c06aa56541cf0b1a4524
Timothy Caro-Bruce <tcarobruce@gmail.com>
2024-12-18 21:16:22 -0800

    19-2

--- 2024/p19.py
+++ 2024/p19.py
@@ -1,5 +1,6 @@
 import sys
 import re
+from functools import cache
 
 f = open(sys.argv[1]).read()
 patterns, towels = f.strip().split("\n\n")
@@ -9,3 +10,15 @@ towels = towels.split("\n")
 
 patt = re.compile("^(" + "|".join(patterns) + ")*$")
 print(len([t for t in towels if patt.match(t)]))
+
+@cache
+def ways(towel):
+    if towel == "":
+        return 1
+    w = 0
+    for pattern in patterns:
+        if towel.startswith(pattern):
+            w += ways(towel[len(pattern):])
+    return w
+
+print(sum([ways(t) for t in towels]))
