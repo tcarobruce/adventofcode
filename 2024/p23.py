2c9a8dec49ea76f64b3b2f7b4a761756a72e9762
Timothy Caro-Bruce <tcarobruce@gmail.com>
2024-12-23 09:46:15 -0800

    single-link

--- 2024/p23.py
+++ 2024/p23.py
@@ -5,26 +5,17 @@ lines = [ln.strip().split("-") for ln in open(sys.argv[1])]
 g = {}
 for a, b in lines:
     a, b = sorted((a, b))
-    g.setdefault(a, []).append(b)
-    g.setdefault(b, []).append(a)
+    g.setdefault(a, set()).add(b)
 
 
 r = []
 for s in g:
     for t in g[s]:
-        if t <= s:
-            continue
-        for u in g[t]:
-            if u <= t or u <= s or u not in g[s]:
+        for u in g.get(t, set()):
+            if u not in g[s]:
                 continue
             tup = [s, t, u]
-            tup.sort()
             r.append(tup)
 
 
-r.sort()
-r = [tup for tup in r if any(m[0] == 't' for m in tup)]
-for tup in r:
-    print(tup)
-
-print(len(r))
+print(sum([any(m[0] == 't' for m in tup) for tup in r]))
