32b1857a991ba60ece2d5b43f37e07370d5fe329
Timothy Caro-Bruce <tcarobruce@gmail.com>
2024-12-23 10:04:30 -0800

    23-2

--- 2024/p23.py
+++ 2024/p23.py
@@ -3,19 +3,29 @@ import sys
 lines = [ln.strip().split("-") for ln in open(sys.argv[1])]
 
 g = {}
+tups = []
 for a, b in lines:
     a, b = sorted((a, b))
+    tups.append((a, b))
     g.setdefault(a, set()).add(b)
 
 
-r = []
-for s in g:
-    for t in g[s]:
-        for u in g.get(t, set()):
-            if u not in g[s]:
-                continue
-            tup = [s, t, u]
-            r.append(tup)
+def augment(tups):
+    result = []
+    for tup in tups:
+        candidates = g.get(tup[0], set()) - set(tup)
+        for c in candidates:
+            if all(c in g.get(t, []) for t in tup):
+                result.append(tup + (c,))
+    return result
 
 
-print(sum([any(m[0] == 't' for m in tup) for tup in r]))
+last = tups
+while tups:
+    print(len(tups[0]), len(tups))
+    if len(tups[0]) == 3:
+        print(sum([any(m[0] == 't' for m in tup) for tup in tups]))
+    tups, last = augment(tups), tups
+
+print(last)
+print(",".join(sorted(last[0])))
