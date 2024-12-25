c3a1f9951aaf0f16d71159a468cc6f3b96df9ac7
Timothy Caro-Bruce <tcarobruce@gmail.com>
2024-12-24 21:45:47 -0800

    20-2 cleaned up, faster

--- 2024/p20.py
+++ 2024/p20.py
@@ -29,48 +29,11 @@ def find_distances(start):
 to_end = find_distances(end)
 from_start = find_distances(start)
 
-def cheats_from(start, cheats):
-    seen = {start}
-    q = [(1, start)]
-    while q:
-        dist, pos = heappop(q)
-        for n in pos.neighbors():
-            if n in seen:
-                continue
-            seen.add(n)
-            d = dist + 1
-            c = grid.get(n)
-            if c is None:
-                continue
-            if c == 'E':
-                yield n, d
-            elif c == '.':
-                yield n, d
-                heappush(q, (d, n))
-            elif c == '#' and d < cheats:
-                heappush(q, (d, n))
 
-
-def cheat_range(start, cheats):
-    to_go = to_end[start]
-    q = deque([(start, 0)])
-    seen = {start}
-    while q:
-        pos, dist = q.popleft()
-        c = grid.get(pos)
-        if c is None:
-            continue
-        if c in '.E' and dist > 0:
-            saved = to_go - dist - to_end[pos]
-            if saved > 0:
-                yield saved
-        if dist == cheats:
-            continue
-        for n in pos.neighbors():
-            if n in seen:
-                continue
-            seen.add(n)
-            q.append((n, dist + 1))
+def manhattan_range(pos, radius):
+    for xoff in range(-radius, radius + 1):
+        for yoff in range(-radius + abs(xoff), radius - abs(xoff) + 1):
+            yield pos + V(xoff, yoff), abs(xoff) + abs(yoff)
 
 
 def find_cheats(cheats):
@@ -83,7 +46,12 @@ def find_cheats(cheats):
         if dist >= max_dist:
             break
 
-        yield from cheat_range(pos, cheats)
+        for cheat_dest, cheat_dist in manhattan_range(pos, cheats):
+            c = grid.get(cheat_dest)
+            if c and c in '.E':
+                saved = to_end[pos] - cheat_dist - to_end[cheat_dest]
+                if saved > 0:
+                    yield saved
 
         for n in pos.neighbors():
             if n in seen:
@@ -92,33 +60,7 @@ def find_cheats(cheats):
             c = grid.get(n)
             if c in '.E':
                 heappush(q, (dist + 1, n))
-            # elif c == "#":
-            #     for cheat_dest, cheat_dist in cheats_from(n, cheats):
-            #         total_dist = dist + cheat_dist + to_end[cheat_dest]
-            #         if total_dist < max_dist and cheat_dest not in seen_from_here:
-            #             #print(pos, cheat_dest, max_dist - total_dist)
-            #             yield max_dist - total_dist
-            #             seen_from_here.add(cheat_dest)
 
-# dirs = [V(2, 0), V(0, 2), V(-2, 0), V(0, -2)]
-
-# ct = Counter()
-# for pos in mainline:
-#     for d in dirs:
-#         n = pos + d
-#         c = grid.get(n)
-#         if c and c in '.E':
-#             l = distances[n] + (nocheat - distances[pos]) + 2
-#             if l < nocheat:
-#                 ct[nocheat - l] += 1
-
-
-# t = 0
-# for savings, count in sorted(ct.items()):
-#     print(count, savings)
-#     if savings >= 100:
-#         t += count
-# print(t)
 tot = 0
 for k, v in sorted(Counter(find_cheats(2)).items()):
     print(v, k)
@@ -126,34 +68,11 @@ for k, v in sorted(Counter(find_cheats(2)).items()):
         tot += v
 print(tot)
 
+lim = 50 if 'sample' in sys.argv[1] else 100
+
 tot = 0
 for k, v in sorted(Counter(find_cheats(20)).items()):
-    if k >= 100:
+    if k >= lim:
         print(v, k)
         tot += v
 print(tot)
-
-
-# def find_paths(cheats_allowed):
-#     q = [(0, start, cheats_allowed, None)]
-
-#     cheats_seen = set()
-
-#     while q:
-#         dist, pos, cheats, cheat_start = heappop(q)
-#         c = grid.get(pos)
-#         if c == 'E' and cheats == cheats_allowed:
-#             return cheats_seen, dist
-
-#         if c == '#':
-#             if cheats == 0:
-#                 continue
-
-#         for n in pos.neighbors():
-#             c = grid.get(n)
-#             if c and c in '.SE':
-#                 heappush(q, (dist + 1, n))
-#                 distances[n] = dist + 1
-#     return distances
-
-
