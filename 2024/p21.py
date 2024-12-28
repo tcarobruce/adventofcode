84b966921a2d8911dada141285248067dbcb4020
Timothy Caro-Bruce <tcarobruce@gmail.com>
2024-12-27 20:12:35 -0800

    21-1 working

--- 2024/p21.py
+++ 2024/p21.py
@@ -23,32 +23,35 @@ def x_legal(key, xoff, level):
         return not (key == "^" or (key == "A" and xoff < -1))
 
 
-def sequence(code, pad, level=0):
+def sequence(code, level=0):
     key = "A"
-    moves = ""
+    result = ""
+    pad = dpad if level else npad
     for next_key in code:
         xoff, yoff = [(t - f) for t, f in zip(pad[next_key], pad[key])]
         xs = abs(xoff) * ('<' if xoff < 0 else '>')
         ys = abs(yoff) * ('^' if yoff < 0 else 'v')
         if x_legal(key, xoff, level):
-            moves += xs + ys
+            moves = [xs + ys + "A", ys + xs + "A"]
         else:
-            moves += ys + xs
-        moves += "A"
+            moves = [ys + xs + "A"]
+        if level == 2:
+            result += moves[0]
+        else:
+            result += min([sequence(seq, level=level+1) for seq in moves], key=lambda s: len(s))
         key = next_key
-    return moves
+    return result
 
 
 def getint(code):
     return int(code.lstrip("0").rstrip("A"))
 
+
 tot = 0
 for code in codes:
-    d1 = sequence(code, npad)
-    d2 = sequence(d1, dpad, 1)
-    d3 = sequence(d2, dpad, 2)
-    for c in [code, d1, d2, d3]:
-        print(c, len(c))
-    tot += len(d3) * getint(code)
+    moves = sequence(code)
+    print(code)
+    print(moves)
+    tot += len(moves) * getint(code)
     print()
 print(tot)
