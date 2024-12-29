8c438ce87dc07361ece51b09113f6930cd0816f2
Timothy Caro-Bruce <tcarobruce@gmail.com>
2024-12-29 10:13:59 -0800

    21-2, finally

--- 2024/p21.py
+++ 2024/p21.py
@@ -14,74 +14,53 @@ dpad = {
     if k != '.'
 }
 
-def x_legal(key, xoff, level):
+def x_can_go_first(key, xoff, level):
     if xoff >= 0:
         return True
     if level == 0:
         return not (key == "0" or (key == "A" and xoff < -1))
     else:
         return not (key == "^" or (key == "A" and xoff < -1))
+
+def y_can_go_first(key, yoff, level):
+    if level == 0:
+        return key not in "147" or "147".find(key) - yoff >= 0
+    else:
+        return key != "<" or yoff >= 0
+
 @cache
 def sequence(code, max_level, level=0):
     key = "A"
-    result = ""
+    result = 0
     pad = dpad if level else npad
-    print(level)
     for next_key in code:
         xoff, yoff = [(t - f) for t, f in zip(pad[next_key], pad[key])]
         xs = abs(xoff) * ('<' if xoff < 0 else '>')
         ys = abs(yoff) * ('^' if yoff < 0 else 'v')
-        if xoff and yoff and x_legal(key, xoff, level):
-            moves = [xs + ys + "A", ys + xs + "A"]
-        else:
+        if not xoff and not yoff:
+            moves = ["A"]
+        elif not xoff:
+            moves = [ys + "A"]
+        elif not yoff:
+            moves = [xs + "A"]
+        elif not x_can_go_first(key, xoff, level):
             moves = [ys + xs + "A"]
-        if level == max_level:
-            result += moves[0]
+        elif not y_can_go_first(key, yoff, level):
+            moves = [xs + ys + "A"]
         else:
-            m = min([sequence(seq, max_level, level=level+1) for seq in moves], key=lambda s: len(s))
-            # if level == 1 and xoff and yoff and x_legal(key, xoff, level) and m != xs + ys + 'A':
-            #     print("DEVIANT", level, key, next_key, xoff, yoff, m)
-            result += m
+            moves = [xs + ys + "A", ys + xs + "A"]
+        if level == max_level:
+            result += len(moves[0])
+        else:
+            result += min([sequence(seq, max_level, level=level+1) for seq in moves])
         key = next_key
     return result
 
-def sequence(init_code, max_level):
-    key = "A"
-    result = ""
-    q = [(init_code, 0, 0)]
-    partner_1 = None
-    while q:
-        print(q)
-        code, level, partner = q.pop()
-        pad = dpad if level else npad
-        code_results = []
-        for next_key in code:
-            xoff, yoff = [(t - f) for t, f in zip(pad[next_key], pad[key])]
-            xs = abs(xoff) * ('<' if xoff < 0 else '>')
-            ys = abs(yoff) * ('^' if yoff < 0 else 'v')
-            if xoff and yoff and x_legal(key, xoff, level):
-                moves = [xs + ys + "A", ys + xs + "A"]
-            else:
-                moves = [ys + xs + "A"]
-            if level == max_level:
-                if partner == 0:
-                    m = moves[0]
-                    if partner_1 and len(partner_1) < len(m):
-                        m = partner_1
-                    result += m
-                    partner_1 = None
-                elif partner == 1:
-                    partner_1 = moves[0]
-            else:
-                for i, seq in enumerate(moves):
-                    q.append((seq, level + 1, i))
-            key = next_key
-    return result
 
 
 def getint(code):
     return int(code.lstrip("0").rstrip("A"))
 
 
-print(sum([len(sequence(code, 2)) * getint(code) for code in codes]))
-#print(sum([len(sequence(code, 5)) * getint(code) for code in codes]))
+print(sum([sequence(code, 2) * getint(code) for code in codes]))
+print(sum([sequence(code, 25) * getint(code) for code in codes]))
