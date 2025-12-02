e8f09354dd46cdd9b108b11252227c8167c26188
Timothy Caro-Bruce <tcarobruce@gmail.com>
2025-12-01 21:01:40 -0800

    nope...

--- 2025/p01.py
+++ 2025/p01.py
@@ -1,12 +1,24 @@
 import sys
 
 pos = 50
-ct = 0
+ct = ct2 = 0
 
 for ln in open(sys.argv[1]).read().split():
-    pos = (pos + {"L": -1, "R": 1}[ln[0]] * int(ln[1:])) % 100
-    ct += pos == 0
+    last = pos
+    pos = pos + {"L": -1, "R": 1}[ln[0]] * int(ln[1:])
+
+    d, pos = divmod(pos, 100)
+    if pos == 0 and d == 0:
+        ct2 += 1
+    elif last == 0 and d == -1:
+        pass
+    else:
+        ct2 += abs(d)
+    print(ln, last, d, pos, ct2)
+
+    if pos == 0:
+        ct += 1
+
 
 print(ct)
-
-
+print(ct2)
