9c04b17f7347a68e161065ddd7f9c6eab2130c13
Timothy Caro-Bruce <tcarobruce@gmail.com>
2025-12-05 07:42:42 -0800

    p5 part 2

--- 2025/p05.py
+++ 2025/p05.py
@@ -21,4 +21,28 @@ for ingredient in ingredients:
 
 print(fresh)
 
+all_range_indexes = []
+for a, b in ranges:
+    all_range_indexes.append((a, 1))
+    all_range_indexes.append((b, -1))
+all_range_indexes.sort(key=lambda x: (x[0], -x[1]))
+
+state = 0
+last_on = None
+total = 0
+for idx, change in all_range_indexes:
+    state += change
+    if state == 0:
+        total += (idx - last_on + 1)
+        #print(idx, last_on, idx-last_on+1)
+        last_on = None
+    elif state == 1 and last_on is None:
+        last_on = idx
+    elif state < 0:
+        print("ERROR, %s at %s" % (state, idx))
+
+print(total)
+
+
+
 
