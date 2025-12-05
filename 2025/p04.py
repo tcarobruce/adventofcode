5a7c9fbdc2b3c0cddf9b45663a78d02247fb9b85
Timothy Caro-Bruce <tcarobruce@gmail.com>
2025-12-05 07:43:04 -0800

    p4 part2

--- 2025/p04.py
+++ 2025/p04.py
@@ -7,16 +7,28 @@ G = readgridv(open(sys.argv[1]))
 maxx = max([v.els[0] for v in G])
 maxy = max([v.els[1] for v in G])
 
-print(G, maxx, maxy)
+p1_done = False
+total = 0
 
-count = 0
+while True:
+    to_remove = set()
+    for g, val in G.items():
+        if val != "@":
+            continue
+        nabes = sum([G.get(n, "") == "@" for n in g.neighbors_diag()])
+        if nabes < 4:
+            to_remove.add(g)
 
-for g, val in G.items():
-    if val != "@":
-        continue
-    nabes = sum([G.get(n, "") == "@" for n in g.neighbors_diag()])
-    if nabes < 4:
-        print(g)
-        count += 1
+    total += len(to_remove)
 
-print(count)
+    if not p1_done:
+        print(total)
+        p1_done = True
+
+    if not to_remove:
+        print(total)
+        break
+
+    for g in to_remove:
+        G[g] = "."
+
